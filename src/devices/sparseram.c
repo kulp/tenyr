098dbc709661722c21fda6e198c035b01ff314fc
Darren Kulp <darren@kulp.ch>
2011-12-18 15:09:14 -0600

    Free up all memory in sparseram fini

--- src/devices/sparseram.c
+++ src/devices/sparseram.c
@@ -16,6 +16,13 @@ struct sparseram_state {
     void *mem;
 };
 
+static int tree_compare(const void *_a, const void *_b)
+{
+    const struct element *a = _a;
+    const struct element *b = _b;
+    return b->addr - a->addr;
+}
+
 static int sparseram_init(struct state *s, void *cookie, ...)
 {
     struct sparseram_state *sparseram = *(void**)cookie = malloc(sizeof *sparseram);
@@ -27,17 +34,37 @@ static int sparseram_init(struct state *s, void *cookie, ...)
 static int sparseram_fini(struct state *s, void *cookie, ...)
 {
     struct sparseram_state *sparseram = cookie;
-    // TODO free tree
+    // tdestroy() is a glibc extension. Here we generate a list of nodes to
+    // delete and then delete them one by one (albeit using a GCC extension,
+    // nested functions, in the process).
+    struct node {
+        void *what;
+        struct node *next;
+    } *todo = NULL;
+
+    void traverse_action(const void *node, VISIT order, int level)
+    {
+        if (order == leaf || order == preorder) {
+            struct node *here = malloc(sizeof *here);
+            here->what = *(void**)node;
+            here->next = todo;
+            todo = here;
+        }
+    }
+
+    twalk(sparseram->mem, traverse_action);
+
+    while (todo) {
+        struct node *temp = todo;
+        todo = todo->next;
+        tdelete(temp->what, &sparseram->mem, tree_compare);
+        free(temp->what);
+        free(temp);
+    }
+
     free(sparseram);
 
     return 0;
-}
-
-static int tree_compare(const void *_a, const void *_b)
-{
-    const struct element *a = _a;
-    const struct element *b = _b;
-    return b->addr - a->addr;
 }
 
 static int sparseram_op(struct state *s, void *cookie, int op, uint32_t addr,
