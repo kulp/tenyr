6693d3b632c6eafefad64ccc48b0eafff86bf9e6
Darren Kulp <darren@kulp.ch>
2012-03-02 23:20:16 -0600

    Tabs -> spaces

--- src/machine.h
+++ src/machine.h
@@ -4,10 +4,10 @@
 #include <stdint.h>
 
 struct machine_state {
-	size_t devices_count;   ///< how many device slots are used
-	size_t devices_max;     ///< how many device slots are allocated
-	struct device **devices;
-	int32_t regs[16];
+    size_t devices_count;   ///< how many device slots are used
+    size_t devices_max;     ///< how many device slots are allocated
+    struct device **devices;
+    int32_t regs[16];
 } machine;
 
 #endif
