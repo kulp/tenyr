e0e2282017d9995dd6a37ce8f9edf5eb61b7486e
Darren Kulp <darren@kulp.ch>
2012-04-18 23:31:10 -0500

    Enable seg7 in icarus sim

--- hw/icarus/icarus_top.v
+++ hw/icarus/icarus_top.v
@@ -1,7 +1,7 @@
 `include "common.vh"
 `timescale 1ms/10us
 
-module Tenyr();
+module Tenyr(output[7:0] seg, output[3:0] an);
     reg halt = 1;
     reg _reset = 0;
     reg clk = 0;
@@ -19,13 +19,19 @@ module Tenyr();
     wire _halt = _reset ? halt : 1'bz;
     always @(negedge clk) halt <= _halt;
 
-    SimMem ram(.clk(clk), .enable(!halt), .p0rw(operand_rw),
+    SimMem #(.BASE(`RESETVECTOR))
+           ram(.clk(clk), .enable(!halt), .p0rw(operand_rw),
                .p0_addr(operand_addr), .p0_data(operand_data),
                .p1_addr(insn_addr)   , .p1_data(insn_data));
 
     SimSerial serial(.clk(clk), ._reset(_reset), .enable(!halt),
                      .rw(operand_rw), .addr(operand_addr),
                      .data(operand_data));
+
+    Seg7 #(.BASE(12'h100))
+             seg7(.clk(clk), ._reset(_reset), .enable(1'b1), // XXX use halt ?
+                  .rw(operand_rw), .addr(operand_addr),
+                  .data(operand_data), .seg(seg), .an(an));
 
     Core core(.clk(clk), ._reset(_reset), .rw(operand_rw),
             .norm_addr(operand_addr), .norm_data(operand_data),
