d5716c7f4fc552cfc157c7f2b7178954a1852c11
Darren Kulp <darren@kulp.ch>
2012-04-30 20:00:46 -0500

    Make Icarus simulation run again

--- hw/icarus/icarus_top.v
+++ hw/icarus/icarus_top.v
@@ -1,10 +1,11 @@
 `include "common.vh"
-`timescale 1ms/10us
+`timescale 1ns/10ps
 
 module Tenyr(output[7:0] seg, output[3:0] an);
     reg reset_n = 0;
     reg rhalt = 1;
     reg clk = 0;
+    wire #(`CLOCKPERIOD / 4) clkL = ~clk;
 
     // TODO halt ?
     always #(`CLOCKPERIOD / 2) clk = ~clk;
@@ -18,6 +19,7 @@ module Tenyr(output[7:0] seg, output[3:0] an);
 
     wire[`HALTBUSWIDTH-1:0] halt;
     assign halt[`HALT_SIM] = rhalt;
+    assign halt[`HALT_TENYR] = rhalt;
 
     SimMem #(.BASE(`RESETVECTOR))
            ram(.clk(clk), .enable(!halt), .p0rw(operand_rw),
@@ -34,7 +36,7 @@ module Tenyr(output[7:0] seg, output[3:0] an);
                   .data(operand_data), .seg(seg), .an(an));
 
     // TODO clkL
-    Core core(.clk(clk), .clkL(clk), .en(1'b1), .reset_n(reset_n), .rw(operand_rw),
+    Core core(.clk(clk), .clkL(clkL), .en(1'b1), .reset_n(reset_n), .rw(operand_rw),
             .norm_addr(operand_addr), .norm_data(operand_data),
             .insn_addr(insn_addr)   , .insn_data(insn_data), .halt(halt));
 endmodule
