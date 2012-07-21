501a940e46c21fedd24ed3ea92493f9304a3d087
Darren Kulp <darren@kulp.ch>
2012-07-21 11:09:58 -0500

    Make spidummy output more verbose

--- src/devices/spidummy.c
+++ src/devices/spidummy.c
@@ -4,25 +4,25 @@
 
 int spidummy_spi_init(void *pcookie)
 {
-	puts(__func__);
+	printf("%s(pcookie=%p)\n", __func__, pcookie);
 	return -1;
 }
 
-int spidummy_spi_select(void *pcookie, int _ss)
+int spidummy_spi_select(void *cookie, int _ss)
 {
-	puts(__func__);
+	printf("%s(cookie=%p, _ss=%i)\n", __func__, cookie, _ss);
 	return -1;
 }
 
 int spidummy_spi_clock(void *cookie, int _ss, int in, int *out)
 {
-	puts(__func__);
+	printf("%s(cookie=%p, _ss=%i, in=%i, out=%p)\n", __func__, cookie, _ss, in, (void*)out);
 	return -1;
 }
 
-int spidummy_spi_fini(void *pcookie)
+int spidummy_spi_fini(void *cookie)
 {
-	puts(__func__);
+	printf("%s(cookie=%p)\n", __func__, cookie);
 	return -1;
 }
 
