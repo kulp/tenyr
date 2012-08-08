b9495dea7df070249a8ec2ce5ae7a341edc4459a
Darren Kulp <darren@kulp.ch>
2012-08-07 21:55:52 -0500

    Update libc too, since bison 2.5 needs it

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-wget http://mirror.pnl.gov/ubuntu//pool/main/b/bison/libbison-dev_2.5.dfsg-2.1_i386.deb
-wget http://mirror.pnl.gov/ubuntu//pool/main/b/bison/bison_2.5.dfsg-2.1_i386.deb
-sudo dpkg -i libbison-dev_2.5.dfsg-2.1_i386.deb
-sudo dpkg -i bison_2.5.dfsg-2.1_i386.deb
+mirror=http://mirror.pnl.gov/ubuntu/
+
+wget $mirror/pool/main/b/bison/libbison-dev_2.5.dfsg-2.1_i386.deb \
+     $mirror/pool/main/b/bison/bison_2.5.dfsg-2.1_i386.deb \
+     $mirror/pool/main/e/eglibc/libc-bin_2.15-0ubuntu10_i386.deb \
+     $mirror/pool/main/e/eglibc/libc6_2.15-0ubuntu10_i386.deb
+
+sudo dpkg -i libc-bin_2.15-0ubuntu10_i386.deb libc6_2.15-0ubuntu10_i386.deb \
+             libbison-dev_2.5.dfsg-2.1_i386.deb bison_2.5.dfsg-2.1_i386.deb
 make
