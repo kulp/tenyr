ed819e6e6121992b7e283b83bdd3cb9d5c936f47
Darren Kulp <darren@kulp.ch>
2012-08-10 18:08:12 -0500

    Simplify test program and start probing for response

--- test/spi.tas.cpp
+++ test/spi.tas.cpp
@@ -5,20 +5,12 @@
 b <- 0
 b -> [(SPI_BASE + 0x14)] // set divider
 // data
-b <- 0
-b <- b | 0      // bits 20 - 31
-b <- b << 12
-b <- b | 0      // bits 8 - 19
-b <- b << 8
-b <- b | 1      // bits 0 - 7
+b <- [reloff(IDLE_COMMAND, 1)]
 b -> [(SPI_BASE + 0x0)] // bits 0 - 31
-b <- 0
-b <- b | 0x400  // bits 32 - 43
-b <- b << 4
-b <- b | 0      // bits 44 - 47
+b <- [reloff(IDLE_COMMAND, 0)]
 b -> [(SPI_BASE + 0x4)] // bits 32 - 47
 
-b <- 48 // length
+b <- (48 + 8) // message length +  response length
 
 //b <- b | (1 << 11) // LSB mode
 
@@ -43,3 +35,6 @@ jnzrel(e,loop)
 b <- [(SPI_BASE + 0x0)] // read data
 c <- [(SPI_BASE + 0x4)] // read data
 illegal
+
+IDLE_COMMAND: // includes response ones
+	.word 0x400000, 0x000001ff
