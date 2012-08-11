b5cc0cb46caffa2323a7921f14aa01f8bddc3c40
Darren Kulp <darren@kulp.ch>
2012-08-11 01:01:38 -0500

    Cosmetics, mend other call site for put_spi

--- test/spi.tas.cpp
+++ test/spi.tas.cpp
@@ -9,6 +9,7 @@
     c -> [(SPI_BASE + 0x18)] // set SS to 1
 
     b <- rel(IDLE_COMMAND)
+    c <- 8
     call(put_spi)
 
 wait_for_sd_ready:
@@ -30,25 +31,22 @@ put_spi:
     // result b is response word
     push(d)
     push(e)
-    push(k)
 
     e <- [b + 1]
     e -> [(SPI_BASE + 0x0)] // bits 0 - 31
     e <- [b + 0]
     e -> [(SPI_BASE + 0x4)] // bits 32 - 56
 
-    k <- (48 + 8) // message length + response length
-
     d <- 1
-    d <- d << 13
-    e <- k | d // ASS mode
+    d <- d << 13 // ASS mode
+    e <- d | c + 48
     e -> [(SPI_BASE + 0x10)]
 
     // GO_BSY
     e <- e | (1 << 8)
     e -> [(SPI_BASE + 0x10)]
 
-    d <- k # wait count
+    d <- c + 48 # wait count
 L_put_spi_clock_wait:
     d <- d - 1
     e <- d <> 0
@@ -61,17 +59,12 @@ L_put_spi_clock_wait:
     d <- ~ d
     b <- b & d
 
-    pop(k)
     pop(e)
     pop(d)
 
     ret
 
-IDLE_COMMAND:
-    /*      01  |cmd-|  |cmd-argument------    ------------------|  |CRC--| 1   |rsp ---| */
-    .word 0b01__000000__0000_0000_0000_0000, 0b0000_0000_0000_0000__0000000_1___1111_1111
-
-RESET_COMMAND:
-    /*      01  |cmd-|  |cmd-argument------    ------------------|  |CRC--| 1   |rsp ---| */
-    .word 0b01__000001__0000_0000_0000_0000, 0b0000_0000_0000_0000__0000000_1___1111_1111
+//                      01  |cmd-|  |cmd-argument------    ------------------|  |CRC--| 1   |rsp ---|
+IDLE_COMMAND:   .word 0b01__000000__0000_0000_0000_0000, 0b0000_0000_0000_0000__0000000_1___1111_1111
+RESET_COMMAND:  .word 0b01__000001__0000_0000_0000_0000, 0b0000_0000_0000_0000__0000000_1___1111_1111
 
