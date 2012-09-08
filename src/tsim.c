011fd9ca69068338b25c4582a12a7c4788eae4f6
Darren Kulp <darren@kulp.ch>
2012-09-08 10:24:22 -0500

    Hoisting plugin-loading code

--- src/tsim.c
+++ src/tsim.c
@@ -71,64 +71,29 @@ static int recipe_abort(struct sim_state *s)
     return 0;
 }
 
-static int recipe_plugin(struct sim_state *s)
+static int plugin_success(void *libhandle, const char *implstem, void *ud)
 {
     int rc = 0;
 
-    const char *implname = NULL;
+    struct sim_state *s = ud;
 
-    int inst = 0;
-    do {
-        char buf[256];
-        snprintf(buf, sizeof buf, "plugin.impl[%d]", inst);
-        if (param_get(&s->conf.params, buf, &implname)) {
-            // If implname contains a slash, treat it as a path ; otherwise, stem
-            const char *implpath = NULL;
-            const char *implstem = NULL;
-            snprintf(buf, sizeof buf, "plugin.impl[%d].stem", inst);
-            param_get(&s->conf.params, buf, &implstem); // may not be set ; that's OK
-            if (strchr(implname, PATH_SEPARATOR_CHAR)) {
-                implpath = implname;
-            } else {
-                snprintf(buf, sizeof buf, ".%clib%s"DYLIB_SUFFIX,
-                        PATH_SEPARATOR_CHAR, implname);
-                buf[sizeof buf - 1] = 0;
-                implpath = buf;
-                if (!implstem)
-                    implstem = implname;
-            }
-
-            // TODO consider using RTLD_NODELETE here
-            // (seems to break on Mac OS X)
-            // currently we leak library handles
-            void *libhandle = dlopen(implpath, RTLD_NOW | RTLD_LOCAL);
-            if (!libhandle) {
-                debug(1, "Could not load %s, trying default library search", implpath);
-                libhandle = RTLD_DEFAULT;
-            }
-
-            tenyr_plugin_host_init(libhandle);
-
-            {
-                char buf[128];
-                snprintf(buf, sizeof buf, "%s_add_device", implstem);
-                void *ptr = dlsym(libhandle, buf);
-                typedef int add_device(struct device **);
-                add_device *adder = ALIASING_CAST(add_device,ptr);
-                if (adder) {
-                    int index = next_device(s);
-                    s->machine.devices[index] = malloc(sizeof *s->machine.devices[index]);
-                    rc |= adder(&s->machine.devices[index]);
-                }
-            }
-
-            // if RTLD_NODELETE worked and were standard, we would dlclose() here
-        } else break;
-
-        inst++;
-    } while (!rc);
+    char buf[128];
+    snprintf(buf, sizeof buf, "%s_add_device", implstem);
+    void *ptr = dlsym(libhandle, buf);
+    typedef int add_device(struct device **);
+    add_device *adder = ALIASING_CAST(add_device,ptr);
+    if (adder) {
+        int index = next_device(s);
+        s->machine.devices[index] = malloc(sizeof *s->machine.devices[index]);
+        rc |= adder(&s->machine.devices[index]);
+    }
 
     return rc;
+}
+
+static int recipe_plugin(struct sim_state *s)
+{
+    return plugin_load("plugin", &s->conf.gops, &s->plugin_cookie, plugin_success, s);
 }
 
 static int recipe_prealloc(struct sim_state *s)
