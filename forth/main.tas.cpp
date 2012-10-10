5f07ac6a01746f96fb332ebbdf8a8be317d1afe3
Darren Kulp <darren@kulp.ch>
2012-10-09 20:35:30 -0500

    Cosmetics ; implement PUTS for counted string

--- forth/main.tas.cpp
+++ forth/main.tas.cpp
@@ -18,75 +18,76 @@ head(start,start): .word
     @NOOP
 
 top: .word
-#if TEST_LOOKUP
-    @TIB, @TO_IN, @FETCH, @ADD,
-    @BL, @WORD,
-    @FIND,
-    IFNOT0(found,notfound),
-#endif
+    #if TEST_LOOKUP
+        @TIB, @TO_IN, @FETCH, @ADD,
+        @BL, @WORD,
+        @FIND,
+        IFNOT0(found,notfound),
+    #endif
 
-    @TIB, @DUP, @TO_IN, @FETCH, @SWAP, @SUB,    // tib used
-    @IN_LEN, @SWAP, @SUB,                       // tib left
-    @ACCEPT,                                    // count
-    @TIB, @TO_IN, @FETCH, @ADD, @ADD, @BL, @SWAP, @STOCHR, //
-    @TIB, @TO_IN, @FETCH, @ADD,
-    @BL, @WORD,
-    @FIND,
-    IFNOT0(found,notfound)
+        @TIB, @DUP, @TO_IN, @FETCH, @SWAP, @SUB,    // tib used
+        @IN_LEN, @SWAP, @SUB,                       // tib left
+        @ACCEPT,                                    // count
+        @TIB, @TO_IN, @FETCH, @ADD, @ADD, @BL, @SWAP, @STOCHR, //
+        @TIB, @TO_IN, @FETCH, @ADD,
+        @BL, @WORD,
+        @FIND,
+        IFNOT0(found,notfound)
 
-strip_spaces: .word
-    @TIB, @TO_IN, @FETCH, @ADD,
-    @FETCHR, @BL, @CMP_EQ,
-    IFNOT0(advance,done_stripping)
-advance: .word
-    @LITERAL, 1, @CHARS, @TO_IN, @ADDMEM,
-    @LITERAL, @strip_spaces, @RELOC, @SET_IP
+    strip_spaces: .word
+        @TIB, @TO_IN, @FETCH, @ADD,
+        @FETCHR, @BL, @CMP_EQ,
+        IFNOT0(advance,done_stripping)
+    advance: .word
+        @LITERAL, 1, @CHARS, @TO_IN, @ADDMEM,
+        @LITERAL, @strip_spaces, @RELOC, @SET_IP
 
-done_stripping: .word
-    @TIB, @TO_IN, @FETCH, @ADD,
-    @FETCHR, @EMIT,
-    @EXIT
-find_word: .word
-    @TIB, @TO_IN, @FETCH, @ADD,
-    @FIND,
-    IFNOT0(found,notfound)
-notfound: .word
-    // TODO complain
-    @LITERAL, 'X', @EMIT,
-    @LITERAL, '\n', @EMIT,
-    @ABORT
+    done_stripping: .word
+        @TIB, @TO_IN, @FETCH, @ADD,
+        @FETCHR, @EMIT,
+        @EXIT
+    find_word: .word
+        @TIB, @TO_IN, @FETCH, @ADD,
+        @FIND,
+        IFNOT0(found,notfound)
+    notfound: .word
+        // TODO complain
+        @LITERAL, @undefined_word, @RELOC,
+        @PUTS,
+        @LITERAL, '\n', @EMIT,
+        @ABORT
 
-found: .word
-    //@DROP, // drop flag for now, assume found
-    @DUP,         @EMIT_UNSIGNED, @BL, @EMIT, @LITERAL, ':', @EMIT, @BL, @EMIT, @CR,
-    @DUP, @RELOC, @EMIT_UNSIGNED, @BL, @EMIT, @LITERAL, ':', @EMIT, @BL, @EMIT, @CR,
-    @RELOC, @EXECUTE,
-	//@EXIT,
+    found: .word
+        //@DROP, // drop flag for now, assume found
+        @DUP,         @EMIT_UNSIGNED, @BL, @EMIT, @LITERAL, ':', @EMIT, @BL, @EMIT, @CR,
+        @DUP, @RELOC, @EMIT_UNSIGNED, @BL, @EMIT, @LITERAL, ':', @EMIT, @BL, @EMIT, @CR,
+        @RELOC, @EXECUTE,
+        //@EXIT,
 
-    // example of a computed branch
-    @LITERAL, 1,
-    IFNOT0(top,bottom)
-bottom: .word
-    @NOOP
+        // example of a computed branch
+        @LITERAL, 1,
+        IFNOT0(top,bottom)
+    bottom: .word
+        @NOOP
 
-wordstart: .word
-    @KEY,
-    @DUP, @LITERAL, '\n', @CMP_EQ,
-    IFNOT0(linedone,checkspace)
-checkspace: .word
-    @DUP, @BL, @CMP_EQ,
-    IFNOT0(worddone,regular),
-    @NOOP
-regular: .word
-    @EMIT,
-    @LITERAL, @wordstart, @RELOC, @SET_IP
-worddone: .word
-    @DROP,
-    @CR,
-    @LITERAL, @wordstart, @RELOC, @SET_IP
-linedone: .word
-    @CR,
-    @EXIT
+    wordstart: .word
+        @KEY,
+        @DUP, @LITERAL, '\n', @CMP_EQ,
+        IFNOT0(linedone,checkspace)
+    checkspace: .word
+        @DUP, @BL, @CMP_EQ,
+        IFNOT0(worddone,regular),
+        @NOOP
+    regular: .word
+        @EMIT,
+        @LITERAL, @wordstart, @RELOC, @SET_IP
+    worddone: .word
+        @DROP,
+        @CR,
+        @LITERAL, @wordstart, @RELOC, @SET_IP
+    linedone: .word
+        @CR,
+        @EXIT
 
 head(IN_LEN,IN_LEN): .word
     @ENTER,
@@ -103,18 +104,44 @@ head(HEXTABLE,HEXTABLE): .word
     @LITERAL, @hexchars, @RELOC,
     @EXIT
 
+undefined_word:
+.L_undefined_word:
+    .word (.L_undefined_word_end - .L_undefined_word - 1)
+    .utf32 "undefined word"
+.L_undefined_word_end:
+
 hexchars:
-    .word '0','1','2','3','4','5','6','7',
-          '8','9','A','B','C','D','E','F'
+    .utf32 "0123456789ABCDEF"
 
 head(TOHEXCHAR,>HEXCHAR): .word
     @ENTER,
     @HEXTABLE, @ADD, @FETCHR,
     @EXIT
 
-head(PUTS,PUTS): .word
+head(PUTS,PUTS): .word // ( c-addr -- )
     @ENTER,
-    // TODO
+    @DUP,                   // c-addr c-addr
+    @FETCHR,                // c-addr count
+    @SWAP,                  // count c-addr
+    @ADD_1CHAR,             // count string
+    @SWAP                   // string count
+
+L_PUTS_top: .word
+    @DUP,                   // string count count
+    @EQZ,                   // string count flag
+    IFNOT0(L_PUTS_done,L_PUTS_emit)
+L_PUTS_emit: .word
+    @SWAP,                  // count string
+    @DUP,                   // count string string
+    @FETCHR,                // count string char
+    @EMIT,                  // count string
+    @ADD_1CHAR,             // count STRING
+    @SWAP,                  // string count
+    @SUB_1,                 // string COUNT
+    GOTO(L_PUTS_top)
+
+L_PUTS_done: .word
+    @TWO_DROP,              // --
     @EXIT
 
 head(SET_IP,SET_IP): .word . + 1
