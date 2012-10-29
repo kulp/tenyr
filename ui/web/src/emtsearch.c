62e27891b0ef0f1fbe075a58789fa2118ac7f1ee
Darren Kulp <darren@kulp.ch>
2012-10-29 00:39:22 -0500

    Replace partial linear tsearch() with full impl
    Matches behaviour of BSD libc at least on Mac OS 10.8. Does not match
    glibc, which uses a self-balancing BST (red-black last time I checked),
    but we do provide GNU's tdestroy().

--- ui/web/src/emtsearch.c
+++ ui/web/src/emtsearch.c
@@ -1,91 +1,127 @@
-// Fake tsearch for emscripten support since emscripten doesn't have one
-// Start with a linear search and upgrade it later if needed
-// We don't provide a working twalk() yet
-#include <stddef.h>
+// tsearch() since emscripten doesn't have one
+// From Algorithms T and D, Knuth, TAoCP Vol. 3 (6.2.2)
+#if EMSCRIPTEN
+// emscripten's includes are not compatible with our prototype for tfind()
+typedef enum { preorder, postorder, endorder, leaf } VISIT;
+#else
+#include <search.h>
+#endif
 #include <stdlib.h>
 
-void *lfind(const void *key, const void *base, size_t *nelp, size_t width, int (*compar)(const void *, const void *));
-void *lsearch(const void *key, void *base, size_t *nelp, size_t width, int (*compar)(const void *, const void *));
-
-struct linear_record {
-    // need a reference to the comparator function in the linear key itself
-    // because there is no properly reentrant way to get it to the lfind()
-    // comparator otherwise
-    int (*foreign)(const void *key1, const void *key2);
-    void *datum;
-    int deleted;    // gets set true after node deletion
+struct tree {
+    // datum must be the first field in struct tree
+    const void *datum;
+    struct tree *left, *right;
 };
 
-struct tree_state {
-    struct linear_record *base;
-    size_t used, allocated;
-};
+static struct tree *null_tree_p = NULL;
 
-static const size_t elt_width = sizeof (struct linear_record);
-
-static int linear_compare(const void *_a, const void *_b)
+// guaranteed to return a non-NULL pointer (might be a pointer to NULL)
+static struct tree **traverse(const void *key, struct tree **rootp, int (*compar)(const void *, const void *), int create, struct tree **parent)
 {
-    const struct linear_record *a = _a,
-                               *b = _b;
+    if (*rootp == NULL) {
+        if (create) {
+            struct tree *q = *rootp = malloc(sizeof *q);
+            q->left = q->right = NULL;
+            q->datum = key;
+            return rootp;
+        } else {
+            return &null_tree_p;
+        }
+    }
 
-    return b->deleted ? -1 : a->foreign(a->datum, b->datum);
+    struct tree *p = *rootp;
+    int result = compar(key, p->datum);
+    if (parent) *parent = p;
+    // we could easily implement this iteratively as well but let's do it
+    // recursively and depend on a smart compiler to use tail recursion
+    if (result == 0) {
+        return rootp;
+    } else if (result < 0) {
+        return traverse(key, &p->left , compar, create, parent);
+    } else {
+        return traverse(key, &p->right, compar, create, parent);
+    }
 }
 
 void *tfind(const void *key, void *const *rootp, int (*compar)(const void *key1, const void *key2))
 {
-    struct tree_state *t = *rootp;
-    struct linear_record k = {
-        .foreign = compar,
-        .datum = (void*)key,
-    };
-    struct linear_record *result = lfind(&k, t->base, &t->used, elt_width, linear_compare);
-    // return a pointer to the datum (so that the user can write through it)
-    return (result && !result->deleted) ? &result->datum : NULL;
+    return *traverse(key, (void*)rootp, compar, 0, NULL);
 }
 
 void *tsearch(const void *key, void **rootp, int (*compar)(const void *key1, const void *key2))
 {
-    if (!*rootp) {
-        struct tree_state *s = *rootp = malloc(sizeof *s);
-        s->used = 0;
-        s->allocated = 64;
-        s->base = malloc(s->allocated * sizeof *s->base);
-    }
-
-    void *result = tfind(key, rootp, compar);
-    if (result) {
-        return result;
-    } else {
-        struct tree_state *t = *rootp;
-        while (t->allocated - t->used < 1) {
-            t->base = realloc(t->base, (t->allocated *= 2) * elt_width);
-        }
-        struct linear_record *k = malloc(sizeof *k);
-        k->foreign = compar;
-        k->datum = (void*)key;
-        k->deleted = 0;
-        struct linear_record *r = lsearch(k, t->base, &t->used, elt_width, linear_compare);
-        return &r->datum;
-    }
+    return *traverse(key, (void*)rootp, compar, 1, NULL);
 }
 
 void *tdelete(const void *restrict key, void **restrict rootp, int (*compar)(const void *key1, const void *key2))
 {
-    struct tree_state *t = *rootp;
-    struct linear_record k = {
-        .foreign = compar,
-        .datum = (void*)key,
-    };
-    struct linear_record *result = lfind(&k, t->base, &t->used, elt_width, linear_compare);
-    result->deleted = 1;
-    // return always a pointer to our "root" and hope the user doesn't care
-    return rootp;
+    struct tree *parent = NULL;
+    struct tree **q = traverse(key, (void*)rootp, compar, 0, &parent);
+    if (!(*q)->datum)
+        return NULL;
+
+    struct tree *t = *q;
+    do {
+        if (!t->right) {
+            *q = t->left;
+            break;
+        }
+
+        struct tree **r = &t->right;
+        if (!(*r)->left) {
+            (*r)->left = t->left;
+            *q = *r;
+            break;
+        }
+
+        struct tree **s = &(*r)->left;
+        while ((*s)->left) {
+            r = s;
+            s = &(*r)->left;
+        }
+        (*s)->left = t->left;
+        (*r)->left = (*s)->right;
+        (*s)->right = t->right;
+        *q = *s;
+    } while (0);
+
+    free(t);
+
+    return parent;
 }
 
-void twalk(const void *root, void (*action) (const void *node, int order, int level))
+// walk(), unlike traverse(), cannot tail-recurse, and so we might want an
+// iterative implementation for large trees
+static void walk(const void *root, void (*action)(const void *node, VISIT order, int level), int level)
 {
-    // TODO
-    (void)root;
-    (void)action;
+    const struct tree *p = root;
+
+    if (!p->left && !p->right) {
+        action(p, leaf, level);
+    } else {
+        action(p, preorder , level);
+        if (p->left ) walk(p->left , action, level + 1);
+        action(p, postorder, level);
+        if (p->right) walk(p->right, action, level + 1);
+        action(p, endorder , level);
+    }
+}
+
+void twalk(const void *root, void (*action)(const void *node, VISIT order, int level))
+{
+    walk(root, action, 0);
+}
+
+void tdestroy(void *root, void (*free_node)(void *))
+{
+    struct tree *p = root;
+    if (!p)
+        return;
+
+    tdestroy(p->left , free_node);
+    tdestroy(p->right, free_node);
+    free_node((void*)p->datum);
+    free(p);
 }
 
