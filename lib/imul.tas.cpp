1972e806413a3d13ddd0cd6c1ad54316677d3d4f
Darren Kulp <darren@kulp.ch>
2013-03-09 10:39:47 -0600

    Support signed numbers in imul

--- lib/imul.tas.cpp
+++ lib/imul.tas.cpp
@@ -5,15 +5,29 @@
 // b -> product
 .global imul
 imul:
-    pushall(h,i)
+    pushall(h,i,j,k)
 
     i <- d == 0
     jnzrel(i, L_done)
+    h <- 1
+    b <- 0
+
+    j <- c >> 31    // save sign bit in j
+    j <- -j         // convert sign to flag
+    c <- c ^ j      // adjust multiplicand
+    c <- c - j
+
+    k <- d >> 31    // save sign bit in k
+    k <- -k         // convert sign to flag
+    d <- d ^ k      // adjust multiplier
+    d <- d - k
+
+    j <- j ^ k      // final flip flag is in j
 
 L_top:
-    h <- d & 1
-    i <- h <> 0
-    i <- c & i
+    // use constant 1 in h to combine instructions
+    i <- d & h - 1
+    i <- c &~ i
     b <- b + i
     c <- c << 1
     d <- d >> 1
@@ -21,6 +35,9 @@ L_top:
     jnzrel(i, L_top)
 
 L_done:
-    popall(h,i)
+    b <- b ^ j      // adjust product for signed math
+    b <- b - j
+
+    popall(h,i,j,k)
     ret
 
