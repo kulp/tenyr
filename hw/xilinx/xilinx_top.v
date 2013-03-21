e3612f6898f766656b209f08ee597fb221c1061d
Darren Kulp <darren@kulp.ch>
2013-03-21 09:14:00 -0500

    Pull out VGA blocks into their own module

--- hw/xilinx/xilinx_top.v
+++ hw/xilinx/xilinx_top.v
@@ -15,19 +15,8 @@ module Tenyr(
     wire oper_rw;
     wire valid_clk, clk_vga, clk_core;
     wire _reset_n = valid_clk & ~reset;
-    wire[31:0] insn_addr, oper_addr;
-    wire[31:0] insn_data, out_data;
+    wire[31:0] insn_addr, oper_addr, insn_data, out_data;
     wire[31:0] oper_data = !oper_rw ? out_data : 32'bz;
-
-    assign vgaRed  [1:0] = {2{vgaRed  [2]}};
-    assign vgaGreen[1:0] = {2{vgaGreen[2]}};
-    assign vgaBlue [1  ] = {1{vgaBlue [2]}};
-
-    tenyr_mainclock clocks(
-        .reset     ( 1'b0     ), .locked       ( valid_clk ), .in ( clk ),
-        .clk_core0 ( clk_core ), .clk_core0_CE ( valid_clk ),
-        .clk_vga   ( clk_vga  ), .clk_vga_CE   ( valid_clk )
-    );
 
     wire `HALTTYPE ihalt;
     assign ihalt[`HALT_TENYR   ] = ~valid_clk;
@@ -36,6 +25,12 @@ module Tenyr(
     assign ihalt[`HALT_SIM     ] = 0;
 `endif
     assign Led[7:0] = {'b0,ihalt};
+
+    tenyr_mainclock clocks(
+        .reset     ( 1'b0     ), .locked       ( valid_clk ), .in ( clk ),
+        .clk_core0 ( clk_core ), .clk_core0_CE ( valid_clk ),
+        .clk_vga   ( clk_vga  ), .clk_vga_CE   ( valid_clk )
+    );
 
     Core core(
         .clk    ( clk_core ), .en     ( valid_clk ), .reset_n ( _reset_n  ),
@@ -69,45 +64,13 @@ module Tenyr(
 `endif
 
 `ifdef VGA
-    wire[11:0] ram_adA, rom_adA;
-    wire[ 7:0] crx, cry, vga_ctl, ram_doA, rom_doA;
-
-    mmr #(.ADDR(`VIDEO_ADDR), .MMR_WIDTH(8), .DEFAULT(8'b11110111)) video_ctl(
-        .clk ( clk_core ), .reset_n ( _reset_n  ), .enable ( 1         ),
-        .rw  ( oper_rw  ), .addr    ( oper_addr ), .data   ( oper_data ),
-        .re  ( 1        ), .we      ( 0         ), .val    ( vga_ctl   )
+    VGAwrap vga(
+        .clk_core ( clk_core ), .clk_vga  ( clk_vga   ),
+        .enable   ( 1        ), .reset_n  ( _reset_n  ),
+        .rw       ( oper_rw  ), .addr     ( oper_addr ), .data    ( oper_data ),
+        .vgaRed   ( vgaRed   ), .vgaGreen ( vgaGreen  ), .vgaBlue ( vgaBlue   ),
+        .hsync    ( hsync    ), .vsync    ( vsync     )
     );
-
-    mmr #(.ADDR(`VIDEO_ADDR + 1), .MMR_WIDTH(8), .DEFAULT(1)) crx_mmr(
-        .clk ( clk_core ), .reset_n ( _reset_n  ), .enable ( 1         ),
-        .rw  ( oper_rw  ), .addr    ( oper_addr ), .data   ( oper_data ),
-        .re  ( 1        ), .we      ( 0         ), .val    ( crx       )
-    ); // crx is 1-based ?
-
-    mmr #(.ADDR(`VIDEO_ADDR + 2), .MMR_WIDTH(8), .DEFAULT(0)) cry_mmr(
-        .clk ( clk_core ), .reset_n ( _reset_n  ), .enable ( 1         ),
-        .rw  ( oper_rw  ), .addr    ( oper_addr ), .data   ( oper_data ),
-        .re  ( 1        ), .we      ( 0         ), .val    ( cry       )
-    ); // cry is 0-based ?
-
-    vga80x40 vga(
-        .clk25MHz ( clk_vga   ), .reset  ( ~_reset_n   ), .octl ( vga_ctl    ),
-        .R        ( vgaRed[2] ), .G      ( vgaGreen[2] ), .B    ( vgaBlue[2] ),
-        .hsync    ( hsync     ), .vsync  ( vsync       ),
-        .TEXT_A   ( ram_adA   ), .TEXT_D ( ram_doA     ),
-        .FONT_A   ( rom_adA   ), .FONT_D ( rom_doA     ),
-        .ocrx     ( crx       ), .ocry   ( cry         )
-    );
-
-    ramwrap #(.BASE(`VIDEO_ADDR + 'h10), .SIZE(80 * 40)) text(
-        .clka  ( clk_vga ), .clkb  ( clk_core  ),
-        .addra ( ram_adA ), .addrb ( oper_addr ),
-        .dina  ( 'bx     ), .dinb  ( oper_data ),
-        .douta ( ram_doA ), .doutb ( oper_data ),
-        .wea   ( 1'b0    ), .web   ( oper_rw   )
-    );
-
-    fontrom font( .clka ( clk_vga ), .addra ( rom_adA ), .douta ( rom_doA ) );
 `endif
 
 endmodule
