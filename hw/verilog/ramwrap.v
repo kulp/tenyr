8f3091e4826e466010983767ac8009a3e0f03fdc
Darren Kulp <darren@kulp.ch>
2013-07-19 22:16:27 -0500

    Expose active signals from ramwrap

--- hw/verilog/ramwrap.v
+++ hw/verilog/ramwrap.v
@@ -3,7 +3,7 @@
 
 // port B is address-offset, and padded to 32 bits
 module ramwrap(
-    input clka, ena, wea, clkb, enb, web,
+    input clka, ena, wea, clkb, enb, web, output reg acta, actb,
     input[PBITS-1:0] addra, input[DBITS-1:0] dina, output[DBITS-1:0] douta,
     input[PBITS-1:0] addrb, input[DBITS-1:0] dinb, output[DBITS-1:0] doutb
 );
@@ -22,21 +22,20 @@ module ramwrap(
 
     wire[DBITS-1:0] douta_internal, doutb_internal;
 
-    assign douta = (a_inrange && !wea) ? douta_internal : 32'bz;
-    assign doutb = (b_inrange && !web) ? doutb_internal : 32'bz;
+    assign douta = (acta && !wea) ? douta_internal : 32'bz;
+    assign doutb = (actb && !web) ? doutb_internal : 32'bz;
 
 `define IN_RANGE(Base,Addr) (Addr[PBITS-1:ABITS] == Base[PBITS-1:ABITS])
 
-    reg a_inrange, b_inrange;
-    always @(posedge clka) a_inrange <= `IN_RANGE(BASE_A,addra);
-    always @(posedge clkb) b_inrange <= `IN_RANGE(BASE_B,addrb);
+    always @(posedge clka) acta <= `IN_RANGE(BASE_A,addra);
+    always @(posedge clkb) actb <= `IN_RANGE(BASE_B,addrb);
 
     BlockRAM #(
         .INIT(INIT), .ZERO(ZERO), .LOAD(LOAD), .LOADFILE(LOADFILE), .SIZE(SIZE),
         .PBITS(ABITS), .ABITS(ABITS), .DBITS(DBITS), .OFFSET(BASE_A)
     ) wrapped(
         .clka  ( clka             ), .clkb  ( clkb             ),
-        .ena   ( ena & a_inrange  ), .enb   ( enb & b_inrange  ),
+        .ena   ( ena & acta       ), .enb   ( enb & actb       ),
         .wea   ( wea              ), .web   ( web              ),
         .addra ( addra[ABITS-1:0] ), .addrb ( addrb[ABITS-1:0] ),
         .dina  ( dina             ), .dinb  ( dinb             ),
