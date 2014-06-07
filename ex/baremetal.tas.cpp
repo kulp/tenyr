a0c9be03c461bb3ac568634c1e90286f86960445
Darren Kulp <darren@kulp.ch>
2014-06-07 13:08:02 -0500

    Hoist common putnum() code

--- ex/baremetal.tas.cpp
+++ ex/baremetal.tas.cpp
@@ -83,30 +83,6 @@ _recurse:
 
     ret
 
-    // putnum takes number in C, (row,col) in D,E, and field width in F
-putnum:
-    pushall(h,i,j,k,l,m)
-    h <- 0x100
-    h <- h << 8         // h is video base
-    k <- d * 80
-    k <- e + k          // k is offset into display
-    k <- k + f          // start at right side of field
-    i <- rel(hexes)     // i is base of hex transform
-
-putnumloop:
-    m <- h + k + 0x10   // m is k characters past start of text region
-    j <- [c & 0xf + i]  // j is character for bottom 4 bits of c
-    [m] <- j            // write character to display
-    k <- k - 1          // go to the left one character
-    c <- c >> 4         // shift down for next iteration
-    l <- c == 0         // shall we loop ?
-    jzrel(l,putnumloop)
-
-putnumdone:
-    popall(h,i,j,k,l,m)
-    ret
-
-
 init_display:
     pushall(h,k,l,m);
     h <- 0x100
@@ -133,9 +109,4 @@ disable_cursor:
     g -> [h << 8]
     popall(g,h)
     ret
-
-
-hexes:
-    .word '0', '1', '2', '3', '4', '5', '6', '7'
-    .word '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 
--- ex/bm_mults.tas.cpp
+++ ex/bm_mults.tas.cpp
@@ -65,28 +65,6 @@ loop_k:
     jnzrel(c,loop_j)
     illegal
 
-    // putnum takes number in C, (row,col) in D,E, and field width in F
-putnum:
-    pushall(h,i,j,k,l,m)
-    h <- 0x100
-    h <- h << 8         // h is video base
-    k <- d * 80 + e     // k is offset into display
-    k <- k + f          // start at right side of field
-    i <- rel(hexes)     // i is base of hex transform
-
-putnumloop:
-    m <- h + k + 0x10   // m is k characters past start of text region
-    j <- [c & 0xf + i]  // j is character for bottom 4 bits of c
-    [m] <- j            // write character to display
-    k <- k - 1          // go to the left one character
-    c <- c >> 4         // shift down for next iteration
-    l <- c <> 0         // shall we loop ?
-    jnzrel(l,putnumloop)
-
-putnumdone:
-    popall(h,i,j,k,l,m)
-    ret
-
 
 init_display:
     pushall(h,k,l,m);
@@ -115,8 +93,4 @@ disable_cursor:
     popall(g,h)
     ret
 
-
-hexes:
-    .word '0', '1', '2', '3', '4', '5', '6', '7'
-    .word '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 
