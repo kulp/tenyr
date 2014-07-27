b125d65ca25ecd615af5cc71ea64fe0f54714f4b
Darren Kulp <darren@kulp.ch>
2014-07-27 16:24:21 -0500

    Simple minutes-seconds clock
    The empirically-determined constants imply that the current implementation in
    Spartan6 executes about 1070*1780*3=5713800 instructions per second.

---
+++ ex/clock.tas.cpp
@@ -0,0 +1,61 @@
+#include "common.th"
+
+_start:
+    bare_metal_init()
+    prologue
+
+top:
+    d <- [rel(min0)]
+    e <- [rel(min1)]
+    f <- [rel(sec0)]
+    g <- [rel(sec1)]
+
+    h <- d << 4 + e
+    h <- h << 4 + f
+    h <- h << 4 + g
+    h -> [0x100]
+
+    c <- 1
+    call(sleep)
+
+    g <- g + 1
+    h <- g == 10
+    g <- g &~ h
+    f <- f - h
+    h <- f == 6
+    f <- f &~ h
+    e <- e - h
+    h <- e == 10
+    e <- e &~ h
+    d <- d - h
+    h <- d == 6
+    d <- d &~ h
+
+    d -> [rel(min0)]
+    e -> [rel(min1)]
+    f -> [rel(sec0)]
+    g -> [rel(sec1)]
+    goto(top)
+
+    illegal
+
+sleep:
+    c <- c * 1070
+    c <- c * 1780
+    call(ticks)
+    ret
+
+ticks:
+    pushall(d,e)
+    d <- 0
+Lticks_loop:
+    e <- d < c
+    d <- d + 1
+    jnzrel(e,Lticks_loop)
+    popall_ret(d,e)
+
+min0: .word 0
+min1: .word 0
+sec0: .word 0
+sec1: .word 0
+
