kulp/tenyr

commit f89e161ed88a27bc72c5ff1855e3d8574d0d7d43

Support type2 instructions

src/asm.c
@@ -87,6 +87,7 @@ int print_disassembly(FILE *out, struct element *i, int flags)
         case 0x5:
         case 0x6:
         case 0x7:
+        case 0x8:
             break;
     }
 
@@ -121,7 +122,7 @@ int print_disassembly(FILE *out, struct element *i, int flags)
     int op3   = g->p ? !opYA : (!!g->imm);
     int op2   = !inert || (g->p ? (g->imm || opYA) : !opYA);
     int op1   = !(opXA && inert) || (!op2 && !op3);
-    int kind  = !!g->p;
+    int kind  = g->p;
 
     if (flags & ASM_VERBOSE)
         op1 = op2 = op3 = hex = 1;
@@ -147,43 +148,59 @@ int print_disassembly(FILE *out, struct element *i, int flags)
         }
     }
 
-    #define C_(E,D,C,B,A) (((E) << 4) | ((D) << 3) | ((C) << 2) | ((B) << 1) | (A))
+    #define C_(E,D,C,B,A) (((E) << 3) | ((D) << 5) | ((C) << 2) | ((B) << 1) | (A))
     // We'd like to use positional references in these format strings, but to
     // preserve compatibility with Win32 libc, we don't.
     // indices : hex, g->p, op1, op2, op3
-    static const char *fmts[C_(1,1,1,1,1)+1] = {
-      //[C_(0,0,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [           ]
-        [C_(0,0,0,0,1)] = "%c%c%c %s %c"                "%d%c", // [Z] <- [         -0]
-        [C_(0,0,0,1,0)] = "%c%c%c %s %c"        "%c"      "%c", // [Z] <- [    Y      ]
-        [C_(0,0,0,1,1)] = "%c%c%c %s %c"        "%c + " "%d%c", // [Z] <- [    Y +  -0]
-        [C_(0,0,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [X          ]
-        [C_(0,0,1,0,1)] = "%c%c%c %s %c%c"        " + " "%d%c", // [Z] <- [X     +  -0]
-        [C_(0,0,1,1,0)] = "%c%c%c %s %c%c %3s " "%c"      "%c", // [Z] <- [X - Y      ]
-        [C_(0,0,1,1,1)] = "%c%c%c %s %c%c %3s " "%c + " "%d%c", // [Z] <- [X - Y +  -0]
-      //[C_(0,1,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [           ]
-        [C_(0,1,0,0,1)] = "%c%c%c %s %c"                "%c%c", // [Z] <- [          Y]
-        [C_(0,1,0,1,0)] = "%c%c%c %s %c"           "%d"   "%c", // [Z] <- [     -0    ]
-        [C_(0,1,0,1,1)] = "%c%c%c %s %c"           "%d + %c%c", // [Z] <- [     -0 + Y]
-        [C_(0,1,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [X          ]
-        [C_(0,1,1,0,1)] = "%c%c%c %s %c%c"           " + %c%c", // [Z] <- [X       + Y]
-        [C_(0,1,1,1,0)] = "%c%c%c %s %c%c %3s "    "%d"   "%c", // [Z] <- [X -  -0    ]
-        [C_(0,1,1,1,1)] = "%c%c%c %s %c%c %3s "    "%d + %c%c", // [Z] <- [X -  -0 + Y]
-      //[C_(1,0,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [           ]
-        [C_(1,0,0,0,1)] = "%c%c%c %s %c"            "0x%08x%c", // [Z] <- [        0x0]
-        [C_(1,0,0,1,0)] = "%c%c%c %s %c"       "%c"       "%c", // [Z] <- [    Y      ]
-        [C_(1,0,0,1,1)] = "%c%c%c %s %c"       "%c + 0x%08x%c", // [Z] <- [    Y + 0x0]
-        [C_(1,0,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [X          ]
-        [C_(1,0,1,0,1)] = "%c%c%c %s %c%c"       " + 0x%08x%c", // [Z] <- [X     + 0x0]
-        [C_(1,0,1,1,0)] = "%c%c%c %s %c%c %3s ""%c"       "%c", // [Z] <- [X - Y      ]
-        [C_(1,0,1,1,1)] = "%c%c%c %s %c%c %3s ""%c + 0x%08x%c", // [Z] <- [X - Y + 0x0]
-      //[C_(1,1,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [           ]
-        [C_(1,1,0,0,1)] = "%c%c%c %s %c"                "%c%c", // [Z] <- [          Y]
-        [C_(1,1,0,1,0)] = "%c%c%c %s %c"       "0x%08x"   "%c", // [Z] <- [    0x0    ]
-        [C_(1,1,0,1,1)] = "%c%c%c %s %c"       "0x%08x + %c%c", // [Z] <- [    0x0 + Y]
-        [C_(1,1,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [X          ]
-        [C_(1,1,1,0,1)] = "%c%c%c %s %c%c"           " + %c%c", // [Z] <- [X       + Y]
-        [C_(1,1,1,1,0)] = "%c%c%c %s %c%c %3s ""0x%08x"   "%c", // [Z] <- [X - 0x0    ]
-        [C_(1,1,1,1,1)] = "%c%c%c %s %c%c %3s ""0x%08x + %c%c", // [Z] <- [X - 0x0 + Y]
+    static const char *fmts[C_(1,2,1,1,1)+1] = {
+      //[C_(0,0,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [            ]
+        [C_(0,0,0,0,1)] = "%c%c%c %s %c"                "%d%c", // [Z] <- [          -0]
+        [C_(0,0,0,1,0)] = "%c%c%c %s %c"        "%c"      "%c", // [Z] <- [     Y      ]
+        [C_(0,0,0,1,1)] = "%c%c%c %s %c"        "%c + " "%d%c", // [Z] <- [     Y +  -0]
+        [C_(0,0,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [ X          ]
+        [C_(0,0,1,0,1)] = "%c%c%c %s %c%c"        " + " "%d%c", // [Z] <- [ X     +  -0]
+        [C_(0,0,1,1,0)] = "%c%c%c %s %c%c %3s " "%c"      "%c", // [Z] <- [ X - Y      ]
+        [C_(0,0,1,1,1)] = "%c%c%c %s %c%c %3s " "%c + " "%d%c", // [Z] <- [ X - Y +  -0]
+      //[C_(0,1,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [            ]
+        [C_(0,1,0,0,1)] = "%c%c%c %s %c"                "%c%c", // [Z] <- [           Y]
+        [C_(0,1,0,1,0)] = "%c%c%c %s %c"           "%d"   "%c", // [Z] <- [      -0    ]
+        [C_(0,1,0,1,1)] = "%c%c%c %s %c"           "%d + %c%c", // [Z] <- [      -0 + Y]
+        [C_(0,1,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [ X          ]
+        [C_(0,1,1,0,1)] = "%c%c%c %s %c%c"           " + %c%c", // [Z] <- [ X       + Y]
+        [C_(0,1,1,1,0)] = "%c%c%c %s %c%c %3s "    "%d"   "%c", // [Z] <- [ X -  -0    ]
+        [C_(0,1,1,1,1)] = "%c%c%c %s %c%c %3s "    "%d + %c%c", // [Z] <- [ X -  -0 + Y]
+      //[C_(0,2,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [            ]
+        [C_(0,2,0,0,1)] = "%c%c%c %s %c"                "%c%c", // [Z] <- [           Y]
+        [C_(0,2,0,1,0)] = "%c%c%c %s %c"        "%c"      "%c", // [Z] <- [     X      ]
+        [C_(0,2,0,1,1)] = "%c%c%c %s %c"        "%c + " "%c%c", // [Z] <- [     X +   Y]
+        [C_(0,2,1,0,0)] = "%c%c%c %s %c%d"                "%c", // [Z] <- [-0          ]
+        [C_(0,2,1,0,1)] = "%c%c%c %s %c%d"        " + " "%c%c", // [Z] <- [-0     +   Y]
+        [C_(0,2,1,1,0)] = "%c%c%c %s %c%d %3s " "%c"      "%c", // [Z] <- [-0 - X      ]
+        [C_(0,2,1,1,1)] = "%c%c%c %s %c%d %3s " "%c + " "%c%c", // [Z] <- [-0 - X +   Y]
+      //[C_(1,0,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [            ]
+        [C_(1,0,0,0,1)] = "%c%c%c %s %c"            "0x%08x%c", // [Z] <- [         0x0]
+        [C_(1,0,0,1,0)] = "%c%c%c %s %c"       "%c"       "%c", // [Z] <- [     Y      ]
+        [C_(1,0,0,1,1)] = "%c%c%c %s %c"       "%c + 0x%08x%c", // [Z] <- [     Y + 0x0]
+        [C_(1,0,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [ X          ]
+        [C_(1,0,1,0,1)] = "%c%c%c %s %c%c"       " + 0x%08x%c", // [Z] <- [ X     + 0x0]
+        [C_(1,0,1,1,0)] = "%c%c%c %s %c%c %3s ""%c"       "%c", // [Z] <- [ X - Y      ]
+        [C_(1,0,1,1,1)] = "%c%c%c %s %c%c %3s ""%c + 0x%08x%c", // [Z] <- [ X - Y + 0x0]
+      //[C_(1,1,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [            ]
+        [C_(1,1,0,0,1)] = "%c%c%c %s %c"                "%c%c", // [Z] <- [           Y]
+        [C_(1,1,0,1,0)] = "%c%c%c %s %c"       "0x%08x"   "%c", // [Z] <- [     0x0    ]
+        [C_(1,1,0,1,1)] = "%c%c%c %s %c"       "0x%08x + %c%c", // [Z] <- [     0x0 + Y]
+        [C_(1,1,1,0,0)] = "%c%c%c %s %c%c"                "%c", // [Z] <- [ X          ]
+        [C_(1,1,1,0,1)] = "%c%c%c %s %c%c"           " + %c%c", // [Z] <- [ X       + Y]
+        [C_(1,1,1,1,0)] = "%c%c%c %s %c%c %3s ""0x%08x"   "%c", // [Z] <- [ X - 0x0    ]
+        [C_(1,1,1,1,1)] = "%c%c%c %s %c%c %3s ""0x%08x + %c%c", // [Z] <- [ X - 0x0 + Y]
+      //[C_(1,0,0,0,0)] = "%c%c%c %s %c"                  "%c", // [Z] <- [            ]
+        [C_(1,2,0,0,1)] = "%c%c%c %s %c"                "%c%c", // [Z] <- [           Y]
+        [C_(1,2,0,1,0)] = "%c%c%c %s %c"       "%c"       "%c", // [Z] <- [       X    ]
+        [C_(1,2,0,1,1)] = "%c%c%c %s %c"       "%c + "  "%c%c", // [Z] <- [       X + Y]
+        [C_(1,2,1,0,0)] = "%c%c%c %s %c0x%08x"            "%c", // [Z] <- [0x0         ]
+        [C_(1,2,1,0,1)] = "%c%c%c %s %c0x%08x"       " + %c%c", // [Z] <- [0x0      + Y]
+        [C_(1,2,1,1,0)] = "%c%c%c %s %c0x%08x %3s ""%c"   "%c", // [Z] <- [0x0 -  X    ]
+        [C_(1,2,1,1,1)] = "%c%c%c %s %c0x%08x %3s ""%c + %c%c", // [Z] <- [0x0 -  X + Y]
     };
 
     // Centre a 1-to-3-character op
@@ -209,6 +226,14 @@ int print_disassembly(FILE *out, struct element *i, int flags)
         case C_(0,1,1,0,1): PUT(f0,f1,f2,f3,f4,f5,      f7,f9); break;
         case C_(0,1,1,1,0): PUT(f0,f1,f2,f3,f4,f5,f6,f8,   f9); break;
         case C_(0,1,1,1,1): PUT(f0,f1,f2,f3,f4,f5,f6,f8,f7,f9); break;
+      //case C_(0,2,0,0,0): PUT(f0,f1,f2,f3,f4,            f9); break;
+        case C_(0,2,0,0,1): PUT(f0,f1,f2,f3,f4,         f7,f9); break;
+        case C_(0,2,0,1,0): PUT(f0,f1,f2,f3,f4,      f5,   f9); break;
+        case C_(0,2,0,1,1): PUT(f0,f1,f2,f3,f4,      f5,f7,f9); break;
+        case C_(0,2,1,0,0): PUT(f0,f1,f2,f3,f4,f8,         f9); break;
+        case C_(0,2,1,0,1): PUT(f0,f1,f2,f3,f4,f8,      f7,f9); break;
+        case C_(0,2,1,1,0): PUT(f0,f1,f2,f3,f4,f8,f6,f5,   f9); break;
+        case C_(0,2,1,1,1): PUT(f0,f1,f2,f3,f4,f8,f6,f5,f7,f9); break;
 
         default:
             fatal(0, "Unsupported hex,kind,op1,op2,op3 %d,%d,%d,%d,%d",

src/sim.c
@@ -7,33 +7,35 @@
 static void do_op(enum op op, int type, int32_t *rhs, uint32_t X, uint32_t Y,
         uint32_t I)
 {
-    int32_t  Xs = X;
-    uint32_t Xu = X;
-    int32_t  Ys = Y;
-    int32_t  Is = SEXTEND32(12, I);
-
-    int32_t  O = (type == 0) ? Ys : Is;
-    int32_t  A = (type == 0) ? Is : Ys;
+    uint32_t p[3] = { 0 };
+    // The `type` determines which position the immediate has
+    p[type] = SEXTEND32(12, I);
+    p[2 - (type > 1)] = X;
+    p[1 - (type > 0)] = Y;
 
     switch (op) {
-        case OP_ADD               : *rhs =  (Xs  +  O) + A; break;
-        case OP_SUBTRACT          : *rhs =  (Xs  -  O) + A; break;
-        case OP_MULTIPLY          : *rhs =  (Xs  *  O) + A; break;
+        #define Ps(x) ((( int32_t*)p)[x])
+        #define Pu(x) (((uint32_t*)p)[x])
+        case OP_ADD               : *rhs =  (Ps(2) +  Ps(1)) + Ps(0); break;
+        case OP_SUBTRACT          : *rhs =  (Ps(2) -  Ps(1)) + Ps(0); break;
+        case OP_MULTIPLY          : *rhs =  (Ps(2) *  Ps(1)) + Ps(0); break;
 
-        case OP_SHIFT_LEFT        : *rhs =  (Xu  << O) + A; break;
-        case OP_SHIFT_RIGHT_LOGIC : *rhs =  (Xu  >> O) + A; break;
-        case OP_SHIFT_RIGHT_ARITH : *rhs =  (Xs  >> O) + A; break;
+        case OP_SHIFT_LEFT        : *rhs =  (Pu(2) << Pu(1)) + Ps(0); break;
+        case OP_SHIFT_RIGHT_LOGIC : *rhs =  (Pu(2) >> Pu(1)) + Ps(0); break;
+        case OP_SHIFT_RIGHT_ARITH : *rhs =  (Ps(2) >> Pu(1)) + Ps(0); break;
 
-        case OP_COMPARE_LT        : *rhs = -(Xs  <  O) + A; break;
-        case OP_COMPARE_EQ        : *rhs = -(Xs  == O) + A; break;
-        case OP_COMPARE_GE        : *rhs = -(Xs  >= O) + A; break;
-        case OP_COMPARE_NE        : *rhs = -(Xs  != O) + A; break;
+        case OP_COMPARE_LT        : *rhs = -(Ps(2) <  Ps(1)) + Ps(0); break;
+        case OP_COMPARE_EQ        : *rhs = -(Ps(2) == Ps(1)) + Ps(0); break;
+        case OP_COMPARE_GE        : *rhs = -(Ps(2) >= Ps(1)) + Ps(0); break;
+        case OP_COMPARE_NE        : *rhs = -(Ps(2) != Ps(1)) + Ps(0); break;
 
-        case OP_BITWISE_AND       : *rhs =  (Xu  &  O) + A; break;
-        case OP_BITWISE_ANDN      : *rhs =  (Xu  & ~O) + A; break;
-        case OP_BITWISE_OR        : *rhs =  (Xu  |  O) + A; break;
-        case OP_BITWISE_XOR       : *rhs =  (Xu  ^  O) + A; break;
-        case OP_BITWISE_XORN      : *rhs =  (Xu  ^ ~O) + A; break;
+        case OP_BITWISE_AND       : *rhs =  (Pu(2) &  Pu(1)) + Ps(0); break;
+        case OP_BITWISE_ANDN      : *rhs =  (Pu(2) &~ Pu(1)) + Ps(0); break;
+        case OP_BITWISE_OR        : *rhs =  (Pu(2) |  Pu(1)) + Ps(0); break;
+        case OP_BITWISE_XOR       : *rhs =  (Pu(2) ^  Pu(1)) + Ps(0); break;
+        case OP_BITWISE_XORN      : *rhs =  (Pu(2) ^~ Pu(1)) + Ps(0); break;
+        #undef Ps
+        #undef Pu
 
         default:
             fatal(0, "Encountered reserved opcode");
@@ -82,7 +84,8 @@ int run_instruction(struct sim_state *s, struct element *i)
         case 0x4:
         case 0x5:
         case 0x6:
-        case 0x7: {
+        case 0x7:
+        case 0x8: {
             struct instruction_general *g = &i->insn.u._0xxx;
             int32_t rhs = 0;
             uint32_t value;

forth/level0.tas.cpp
@@ -215,7 +215,7 @@ head(FILL,FILL):
     T2 <- [S + 3]       // T2 is address
     T3 <- 0             // T3 is offset
 L_FILL_top:
-    T4 <- T1 >= 1       // T4 is loop condition
+    T4 <- 0 < T1        // T4 is loop condition
     iffalse(T4, L_FILL_done)
     T0 -> [T2 + T3]     // write char to location
     T3 <- T3 + 1        // increment offset

lib/baremetal_common.tas.cpp
@@ -12,7 +12,7 @@ init_display_loop:
     m <- h + k + 0x10   // m is k characters past start of text region
     [m] <- ' '          // write space to display
     k <- k + 1          // go to the right one character
-    l <- k >= 0x21      // shall we loop ?
+    l <- 0x20 < k       // shall we loop ?
     jzrel(l,init_display_loop)
 
 init_display_done:

lib/fp/fp_mul.tas.cpp
@@ -46,7 +46,7 @@ fp_mul:
   m <- 1
   m <- m << 23
   m <- m & c
-  m <- m >= 1
+  m <- 0 < m
   jnzrel(m, no_shift)
   
   // Adjust exponent.

lib/strtol.tas.cpp
@@ -11,7 +11,7 @@ strtol:
     callnz(h,detect_base)
     e <- b
 
-    h <- e >= 37
+    h <- 36 < e
     jnzrel(h,strtol_error_EINVAL)
     h <- e < 2
     jnzrel(h,strtol_error_EINVAL)

src/ops.h
@@ -24,6 +24,14 @@
 //      01 :  Z  <- [X f I + Y]
 //      11 :  Z  -> [X f I + Y]
 //
+// [10DDZZZZXXXXYYYYffffIIIIIIIIIIII]
+// I = sign-extended 12-bit immediate
+// performs
+// DD = 00 :  Z  <-  I f X + Y
+//      10 : [Z] <-  I f X + Y
+//      01 :  Z  <- [I f X + Y]
+//      11 :  Z  -> [I f X + Y]
+//
 //  a <- [b * c + 4]
 //  a <- [p + 3]
 //  p <- [p + c] (jump from table)
@@ -71,8 +79,7 @@ struct insn_or_data {
             unsigned x   :  4;  ///< operand x
             unsigned z   :  4;  ///< operand z
             unsigned dd  :  2;  ///< dereference
-            unsigned p   :  1;  ///< expr type0 or type1
-            unsigned t   :  1;  ///< type bit
+            unsigned p   :  2;  ///< expr type0 or type1
         } _0xxx;
     } u;
     uint32_t reladdr;   ///< used for CE_ICI resolving

src/parser.y
@@ -15,10 +15,8 @@ static struct const_expr *add_deferred_expr(struct parse_data *pd, struct
         const_expr *ce, int mult, uint32_t *dest, int width, int flags);
 static struct const_expr *make_const_expr(enum const_expr_type type, int op,
         struct const_expr *left, struct const_expr *right, int flags);
-static struct expr *make_expr_type0(int x, int op, int y, int mult, struct
-        const_expr *defexpr);
-static struct expr *make_expr_type1(int x, int op, struct const_expr *defexpr,
-        int y);
+static struct expr *make_expr(int type, int x, int op, int y, int mult, struct
+		const_expr *defexpr);
 static struct expr *make_unary_type0(int x, int op, int mult, struct const_expr
         *defexpr);
 static struct element *make_insn_general(struct parse_data *pd, struct
@@ -107,7 +105,7 @@ extern void tenyr_pop_state(void *yyscanner);
 %type <program> program ascii utf32 data string_or_data
 %type <str> symbol symbol_list
 
-%expect 2
+%expect 3
 
 %union {
     int32_t i;
@@ -203,8 +201,8 @@ insn_inner
             free($rhs_plain);
             free($lhs_plain); }
     | lhs_plain tor rhs_plain
-        {   struct expr *t0 = make_expr_type0($rhs_plain->x, OP_BITWISE_OR, 0, 0, NULL),
-                        *t1 = make_expr_type0($lhs_plain->x, OP_BITWISE_OR, 0, 0, NULL);
+        {   struct expr *t0 = make_expr(0, $rhs_plain->x, OP_BITWISE_OR, 0, 0, NULL),
+                        *t1 = make_expr(0, $lhs_plain->x, OP_BITWISE_OR, 0, 0, NULL);
             $insn_inner = make_insn_general(pd, t0, 0, t1);
             free(t1);
             free(t0);
@@ -278,11 +276,11 @@ lhs_deref
 rhs_plain
     /* type0 */
     : regname[x] op regname[y] reloc_op greloc_expr
-        { $rhs_plain = make_expr_type0($x, $op, $y, $reloc_op == '+' ? 1 : -1, $greloc_expr); }
+        { $rhs_plain = make_expr(0, $x, $op, $y, $reloc_op == '+' ? 1 : -1, $greloc_expr); }
     | regname[x] op regname[y]
-        { $rhs_plain = make_expr_type0($x, $op, $y, 0, NULL); }
+        { $rhs_plain = make_expr(0, $x, $op, $y, 0, NULL); }
     | regname[x]
-        { $rhs_plain = make_expr_type0($x, OP_BITWISE_OR, 0, 0, NULL); }
+        { $rhs_plain = make_expr(0, $x, OP_BITWISE_OR, 0, 0, NULL); }
     | unary_op regname[x] reloc_op greloc_expr
         { $rhs_plain = make_unary_type0($x, $unary_op, $reloc_op == '+' ? 1 : -1, $greloc_expr); }
     | unary_op regname[x]
@@ -291,14 +289,19 @@ rhs_plain
         {   /* When the RHS is just a constant, choose OR or ADD depending on
              * what kind of constant this is likely to be. */
             enum op op = ($greloc_expr->flags & IMM_IS_BITS) ? OP_BITWISE_OR : OP_ADD;
-            $rhs_plain = make_expr_type0(0, op, 0, 1, $greloc_expr); }
+            $rhs_plain = make_expr(0, 0, op, 0, 1, $greloc_expr); }
     /* type1 */
     | regname[x] op greloc_expr '+' regname[y]
-        { $rhs_plain = make_expr_type1($x, $op, $greloc_expr, $y); }
+        { $rhs_plain = make_expr(1, $x, $op, $y, 1, $greloc_expr); }
     | regname[x] op greloc_expr
-        { $rhs_plain = make_expr_type1($x, $op, $greloc_expr, 0); }
+        { $rhs_plain = make_expr(1, $x, $op, 0, 1, $greloc_expr); }
     | greloc_expr '+' regname[y]
-        { $rhs_plain = make_expr_type1(0, OP_ADD, $greloc_expr, $y); }
+        { $rhs_plain = make_expr(1, 0, OP_ADD, $y, 1, $greloc_expr); }
+    /* type2 */
+    | greloc_expr op regname[x] '+' regname[y]
+        { $rhs_plain = make_expr(2, $x, $op, $y, 1, $greloc_expr); }
+    | greloc_expr op regname[x]
+        { $rhs_plain = make_expr(2, $x, $op, 0, 1, $greloc_expr); }
 
 unary_op
     : '~' { $unary_op = OP_BITWISE_XORN; }
@@ -465,7 +468,6 @@ static struct element *make_insn_general(struct parse_data *pd, struct
 
     int dd = ((lhs->deref | !!arrow) << 1) | expr->deref;
 
-    elem->insn.u._0xxx.t   = 0;
     elem->insn.u._0xxx.p   = expr->type;
     elem->insn.u._0xxx.dd  = dd;
     elem->insn.u._0xxx.z   = lhs->x;
@@ -517,38 +519,18 @@ static struct const_expr *make_const_expr(enum const_expr_type type, int op,
     return n;
 }
 
-static struct expr *make_expr_type0(int x, int op, int y, int mult, struct
-        const_expr *defexpr)
+static struct expr *make_expr(int type, int x, int op, int y, int mult, struct
+		const_expr *defexpr)
 {
     struct expr *e = calloc(1, sizeof *e);
 
-    e->type  = 0;
+    e->type  = type;
     e->deref = 0;
     e->x     = x;
     e->op    = op;
     e->y     = y;
     e->mult  = mult;
     e->ce    = defexpr;
-    if (defexpr)
-        e->i = 0xfffffbad; // put in a placeholder that must be overwritten
-    else
-        e->i = 0; // there was no const_expr ; zero defined by language
-
-    return e;
-}
-
-static struct expr *make_expr_type1(int x, int op, struct const_expr *defexpr,
-        int y)
-{
-    struct expr *e = calloc(1, sizeof *e);
-
-    e->type  = 1;
-    e->deref = 0;
-    e->x     = x;
-    e->op    = op;
-    e->ce    = defexpr;
-    e->mult  = 1;
-    e->y     = y;
     if (defexpr)
         e->i = 0xfffffbad; // put in a placeholder that must be overwritten
     else

commit e1fc5e04b689b7bd708789e7ec7a5e037122cff6

Mend brokenness revealed by 0489404
The bug was introduced in f89e161 but not discovered until 0489404 revealed it
: type2 instructions containing dereferences were being processed as invalid.
This was caught by `bsearch_demo.texe` in `make check`.

src/asm.c
@@ -88,6 +88,9 @@ int print_disassembly(FILE *out, struct element *i, int flags)
         case 0x6:
         case 0x7:
         case 0x8:
+        case 0x9:
+        case 0xa:
+        case 0xb:
             break;
     }
 

src/sim.c
@@ -85,7 +85,10 @@ int run_instruction(struct sim_state *s, struct element *i)
         case 0x5:
         case 0x6:
         case 0x7:
-        case 0x8: {
+        case 0x8:
+        case 0x9:
+        case 0xa:
+        case 0xb: {
             struct instruction_general *g = &i->insn.u._0xxx;
             int32_t rhs = 0;
             uint32_t value;