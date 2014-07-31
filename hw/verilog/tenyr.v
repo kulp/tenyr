04c91353255713bbc4ff220ac5fa70f6630c63e1
Darren Kulp <darren@kulp.ch>
2014-07-31 07:28:45 -0500

    Lots of cosmetic changes, and Shuf gets .en()
    These cosmetic changes are pretty pervasive, as well as unnecessary.

--- hw/verilog/tenyr.v
+++ hw/verilog/tenyr.v
@@ -1,18 +1,18 @@
 `include "common.vh"
 `timescale 1ns/10ps
 
-module Reg(input clk, upZ,               input[ 3:0] indexZ, indexX, indexY,
-           input[31:0] writeZ, next_pc, output[31:0] valueZ, valueX, valueY);
+module Reg(input clk, upZ,             input[ 3:0] idxZ, idxX, idxY,
+           input[31:0] nextZ, nextP, output[31:0] valZ, valX, valY);
 
     reg[31:0] store[0:14];
     initial store[0] = 0;
 
-    //              reg is P ?           reg is B-O
-    assign valueX = &indexX ? next_pc : store[indexX];
-    assign valueY = &indexY ? next_pc : store[indexY];
-    assign valueZ = &indexZ ? next_pc : store[indexZ];
+    //            reg is P ?      reg is B-O
+    assign valX = &idxX ? nextP : store[idxX];
+    assign valY = &idxY ? nextP : store[idxY];
+    assign valZ = &idxZ ? nextP : store[idxZ];
 
-    always @(posedge clk) if (&{upZ,|indexZ,~&indexZ}) store[indexZ] <= writeZ;
+    always @(posedge clk) if (&{upZ,|idxZ,~&idxZ}) store[idxZ] <= nextZ;
 
 endmodule
 
@@ -27,11 +27,11 @@ module Decode(input[31:0] insn, output[3:0] Z, X, Y, output[11:0] I,
 
 endmodule
 
-module Shuf(input clk, input[1:0] kind, input[31:0] X, Y, input[11:0] I,
-                                   output reg[31:0] A, B, C);
+module Shuf(input clk, input en, input[1:0] kind,
+            input[31:0] X, Y, input[11:0] I, output reg[31:0] A, B, C);
 
     wire[31:0] J = { {20{I[11]}}, I[11:0] };
-    always @(posedge clk)
+    always @(posedge clk) if (en)
         case (kind)
             2'b00   : begin A <= X   ; B <= Y   ; C <= J   ; end
             2'b01   : begin A <= X   ; B <= J   ; C <= Y   ; end
@@ -74,13 +74,12 @@ module Core(input clk, reset_n, trap, inout wor `HALTTYPE halt, output strobe,
                     sE=8, sF=9, sR=10, sT=11, sW=12;
 
     wire throw, kind, drhs, jumping, storing, loading, deref;
-    wire[ 3:0] indexX, indexY, indexZ, op;
-    wire[31:0] valueX, valueY, valueZ, irhs, rhs, tostore;
-    wire[31:0] valueA, valueB, valueC;
-    wire[11:0] valueI;
+    wire[ 3:0] idxX, idxY, idxZ, op;
+    wire[31:0] valX, valY, valZ, valA, valB, valC, irhs, rhs, tostore;
+    wire[11:0] valI;
     wire[ 4:0] vector;
 
-    reg [31:0] r_irhs, r_data, next_pc, v_addr, insn;
+    reg [31:0] r_irhs, r_data, nextP, v_addr, insn;
     reg [3:0] state = sI;
 
     always @(posedge clk)
@@ -92,8 +91,8 @@ module Core(input clk, reset_n, trap, inout wor `HALTTYPE halt, output strobe,
             s2: begin state <= s3; r_irhs  <= irhs;                     end
             s3: begin state <= s4; /* compensate for slow multiplier */ end
             s4: begin state <= s5; r_data  <= d_in;                     end
-            s5: begin state <= s6; i_addr  <= jumping ? rhs : next_pc;  end
-            s6: begin state <= s7; next_pc <= i_addr + 1;               end
+            s5: begin state <= s6; i_addr  <= jumping ? rhs : nextP;    end
+            s6: begin state <= s7; nextP   <= i_addr + 1;               end
             s7: begin state <= s0; insn    <= i_data; /* why extra ? */ end
             // TODO return to instruction OF a trap, but AFTER a throw
             sE: begin state <= sR; r_irhs  <= `VECTOR_ADDR | vector;    end
@@ -107,36 +106,36 @@ module Core(input clk, reset_n, trap, inout wor `HALTTYPE halt, output strobe,
     // Instruction fetch happens on cycle 0
 
     // Decode and register reads happen as soon as instruction is ready
-    Decode decode(.Z ( indexZ ), .insn  ( insn   ), .storing   ( storing ),
-                  .X ( indexX ), .kind  ( kind   ), .deref_rhs ( drhs    ),
-                  .Y ( indexY ), .op    ( op     ), .branch    ( jumping ),
-                  .I ( valueI ), .throw ( throw  ), .vector    ( vector  ));
+    Decode decode(.Z ( idxZ ), .insn  ( insn   ), .storing   ( storing ),
+                  .X ( idxX ), .kind  ( kind   ), .deref_rhs ( drhs    ),
+                  .Y ( idxY ), .op    ( op     ), .branch    ( jumping ),
+                  .I ( valI ), .throw ( throw  ), .vector    ( vector  ));
 
     // Shuffle occurs on cycle 0
-    Shuf shuf(.clk ( clk  ), .A( valueA ), .B( valueB ), .C( valueC ),
-              .kind( kind ), .X( valueX ), .Y( valueY ), .I( valueI ));
+    Shuf shuf(.clk  ( clk         ), .X ( valX ), .A ( valA ),
+              .en   ( state == s0 ), .Y ( valY ), .B ( valB ),
+              .kind ( kind        ), .I ( valI ), .C ( valC ));
 
     // Execution (arithmetic operation) occurs on cycle 1
-    wire en_ex = state == s1;
-    Exec exec(.clk ( clk    ), .en ( en_ex  ), .op ( op     ),
-              .A   ( valueA ), .B  ( valueB ), .C  ( valueC ), .rhs ( irhs ));
+    Exec exec (.clk ( clk         ), .op  ( op   ), .A ( valA ),
+               .en  ( state == s1 ), .rhs ( irhs ), .B ( valB ),
+                                                    .C ( valC ));
 
-    // Memory loads or stores on cycle 3
+    // Memory loads or stores on cycle 4
     assign loading = (drhs && !mem_rw) || state == sR;
     assign strobe  = (state == s4 && (loading || storing)) || state == sW;
     assign mem_rw  = storing || state == sW;
-    assign tostore = state == sW ? `TRAP_ADDR : valueZ;
+    assign tostore = state == sW ? `TRAP_ADDR : valZ;
     assign deref   = drhs && (state != sT);
     assign rhs     = deref ? r_data : r_irhs;
-    assign d_out   = deref ? valueZ : r_irhs;
+    assign d_out   = deref ? valZ   : r_irhs;
     assign d_addr  = deref ? r_irhs : tostore;
 
-    // Registers commit on cycle 4
-    wire updateZ = !storing && state == s5;
-    Reg regs(.clk     ( clk     ), .indexX ( indexX ), .valueX ( valueX  ),
-             .next_pc ( next_pc ), .indexY ( indexY ), .valueY ( valueY  ),
-                                   .indexZ ( indexZ ), .valueZ ( valueZ  ),
-                                   .writeZ ( rhs    ), .upZ    ( updateZ ));
+    // Registers commit on cycle 5
+    wire upZ = !storing && state == s5;
+    Reg regs(.clk   ( clk   ),               .idxX ( idxX ), .valX ( valX ),
+             .nextP ( nextP ),               .idxY ( idxY ), .valY ( valY ),
+             .nextZ ( rhs   ), .upZ ( upZ ), .idxZ ( idxZ ), .valZ ( valZ ));
 
 endmodule
 
