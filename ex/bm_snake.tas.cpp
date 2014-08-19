0336fc71031717030e65d65ca7b14f9d9d8297b0
Darren Kulp <darren@kulp.ch>
2014-08-18 21:29:59 -0500

    Add delay in bm_snake for hardware

--- ex/bm_snake.tas.cpp
+++ ex/bm_snake.tas.cpp
@@ -92,6 +92,14 @@ L_snake:
     e <- rel(snakes_after)
     j <- n < e
     jnzrel(j,L_snake)
+#ifndef SIM
+	j <- 250
+	j <- j * 75
+slow_down:
+	e <- j > 0
+	j <- j - 1
+	jnzrel(e,slow_down)
+#endif
     goto(L_loop) // infinite loop
 
 snakes:
