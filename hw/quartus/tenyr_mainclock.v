15b1fe2cec9c8401ca2fea870e1fd43711a5250e
Darren Kulp <darren@kulp.ch>
2014-08-26 12:11:20 -0500

    Mend clock issues
    The clk_core0 must be a register
    
    Add preliminary clock constraints

--- hw/quartus/tenyr_mainclock.v
+++ hw/quartus/tenyr_mainclock.v
@@ -1,18 +1,14 @@
 // For a 50MHz input
-// Produces a 50MHz clk_core0 and a 25MHz clk_vga
-// review clk_core0 for potentially bad gating
+// Produces a 50MHz clk_core and a 25MHz clk_vga
 module tenyr_mainclock(
-    input in, reset, output locked,
-    output clk_core0, input clk_core0_CE, output reg clk_vga, input clk_vga_CE
+    input clk_in, reset, output locked,
+    output clk_core, output reg clk_vga
 );
     assign locked = 1;
+    assign clk_core = clk_in;
 
-    always @(posedge in) begin
-        if (clk_vga_CE)
-            clk_core0 <= in;
-        if (clk_vga_CE)
-            clk_vga <= ~clk_vga;
-    end
+    always @(posedge clk_in)
+        clk_vga <= ~clk_vga;
 
 endmodule
 
