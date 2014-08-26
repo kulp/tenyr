fe6ddee52d36868c707d38a5c418508af554a2fb
Darren Kulp <darren@kulp.ch>
2014-08-26 12:53:00 -0500

    No multidim arrays in seg7 ; simplification

--- hw/verilog/seg7.v
+++ hw/verilog/seg7.v
@@ -11,29 +11,33 @@ module Seg7(input clk, strobe, rw, reset_n, input[31:0] addr, data,
     localparam      DIGITS   = 4;
 
     reg[CNT_BITS-1:0] counter = 0;
-    reg[DIGITS-1:0][3:0] store[1:0];
+    reg[DIGITS*4-1:0] store;
+    reg[3:0] dots;
     reg[1:0] dig = 0;
 
     wire in_range = addr[31:1] == BASE[31:1];
-    assign seg[7] = (store[1][0] & ~an) == 0;
-    Hex2Segments lookup(clk, store[0][dig], seg[6:0]);
+    assign seg[7] = (dots[0 +: 4] & ~an) == 0;
+    Hex2Segments lookup(clk, store[dig * 4 +: 4], seg[6:0]);
 
     initial an <= ~1;
 
     always @(posedge clk) begin
         if (!reset_n) begin
-            an       <= ~1;
-            dig      <= 0;
-            counter  <= 0;
-            store[0] <= 0;
-            store[1] <= 0;
+            an      <= ~1;
+            dig     <= 0;
+            counter <= 0;
+            store   <= 0;
+            dots    <= 0;
         end else begin
             counter <= counter + 1;
             if (&counter) begin
                 an  <= {an[DIGITS-2:0],an[DIGITS-1]};
                 dig <= dig + 1;
             end else if (in_range && strobe && rw)
-                store[addr[0]] <= data;
+                case (addr[0])
+                    1'b0: store <= data;
+                    1'b1: dots  <= data;
+                endcase
         end
     end
 
