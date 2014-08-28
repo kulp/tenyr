dcaf578e5f5ce64d5bab748534594e73fa7d7372
Darren Kulp <darren@kulp.ch>
2014-08-27 20:59:32 -0500

    Improve (correct) tas-dogfooding script

--- scripts/dogfood.sh
+++ scripts/dogfood.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-here=`dirname $0`
+here=`dirname $BASH_SOURCE`
 stem=`basename $1`
+shift
 tas=$here/../tas
 base=`mktemp -t $stem`
-temp1=$base.1.tas
-temp2=$base.2.tas
-temp3=$base.3.tas
 pp="cpp -I$here/../lib"
-$pp $1 | $tas - | $tas -d -v - | tee $temp1 | $tas - | $tas -d -v - | tee $temp2 | $tas - | $tas -d -v -o $temp3 -
-diff -u $temp1 $temp2
-diff -u $temp2 $temp3
+for file in $* ; do
+	temp1=$base.1.tas
+	temp2=$base.2.tas
+	temp3=$base.3.tas
+	trap "rm $temp1 $temp2 $temp3" EXIT
+	$pp $file | $tas - | $tas -d -v - | tee $temp1 | $tas - | $tas -d -v - | tee $temp2 | $tas - | $tas -d -v -o $temp3 -
+	diff -q $temp1 $temp2 && diff -q $temp2 $temp3 && echo $(basename $file): OK || echo $(basename $file): FAILED
+done
