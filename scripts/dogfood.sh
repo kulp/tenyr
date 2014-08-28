eaa49a1b0e503599cfb4301fcc27bd6f4d06ac77
Darren Kulp <darren@kulp.ch>
2014-08-27 21:06:13 -0500

    Don't use the preprocessor unless we have a .tas.cpp
    Otherwise, we could get errors on valid #-comments.

--- scripts/dogfood.sh
+++ scripts/dogfood.sh
@@ -4,12 +4,16 @@ stem=`basename $1`
 shift
 tas=$here/../tas
 base=`mktemp -t $stem`
-pp="cpp -I$here/../lib"
 for file in $* ; do
 	temp1=$base.1.tas
 	temp2=$base.2.tas
 	temp3=$base.3.tas
 	trap "rm $temp1 $temp2 $temp3" EXIT
+	if [[ $file = *.cpp ]] ; then
+		pp="cpp -I$here/../lib"
+	else
+		pp=cat
+	fi
 	$pp $file | $tas - | $tas -d -v - | tee $temp1 | $tas - | $tas -d -v - | tee $temp2 | $tas - | $tas -d -v -o $temp3 -
 	diff -q $temp1 $temp2 && diff -q $temp2 $temp3 && echo $(basename $file): OK || echo $(basename $file): FAILED
 done
