aae35782607cf202fe9b0f7280d4c7e5b3553fa9
Darren Kulp <darren@kulp.ch>
2014-08-29 17:58:42 -0500

    Mend sim clocks

--- hw/verilog/sim/simclocks.v
+++ hw/verilog/sim/simclocks.v
@@ -2,14 +2,13 @@
 `timescale 1ms/10us
 
 module `SIMCLK(
-	input in, reset, output locked,
-	output clk_core0, input clk_core0_CE,
-	output clk_vga, input clk_vga_CE
+	input clk_in, reset, output locked,
+	output clk_core, clk_vga
 );
 
-	assign locked    = ~reset;
-	assign clk_core0 = in & clk_core0_CE;
-	assign clk_vga   = in & clk_vga_CE;
+	assign locked   = ~reset;
+	assign clk_core = clk_in;
+	assign clk_vga  = clk_in;
 
 endmodule
 
