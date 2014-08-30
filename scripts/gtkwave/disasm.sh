0823640851a4750601f17d67112ac4b09a047b89
Darren Kulp <darren@kulp.ch>
2014-08-29 20:47:54 -0500

    Simplify and robustify disasm.sh
    Now we keep a single instance open as long as it gets valid data (and
    restart it once it doesn't). This should improve gtkwave responsiveness
    and reduce CPU load. This necessitated the line-buffering of the output
    through `tr` and `sed`.
    
    Also shorten strings of leading zeros in hexadecimal constants.

--- scripts/gtkwave/disasm.sh
+++ scripts/gtkwave/disasm.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 here=$(dirname $0)
 tas="$(make -C $here/../.. showbuilddir)/tas"
-while read b ; do
-    if [[ $b == "" ]]; then
-        echo "EMPTY"
-    else
-        # TODO rewrite to avoid having to start tas for every new line.
-        # Currently we do this to avoid buffering problems.
-        out=$($tas --quiet --disassemble --format text - <<<$b | tr -s ' ' | sed 's/^ //')
-        if [[ $? == 0 ]]; then
-            echo "$out"
-        else
-            echo "BAD"
-        fi
-    fi
+# We have to run a loop even in the error condition, to keep gtkwave from hanging
+[[ -x $tas ]] || { while read ; do echo "tas not found" ; done ; exit 1; }
+while true ; do
+	$tas --quiet --disassemble --format text - | tr -u -s ' ' | sed -le 's/^ //; s/0x0*/0x/'
+	echo BAD
 done
