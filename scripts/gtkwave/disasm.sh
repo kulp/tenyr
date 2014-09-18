88ba94855e584d23d2ff59ea46f0bbf38e9fa01e
Darren Kulp <darren@kulp.ch>
2014-09-17 22:33:59 -0500

    Hide chaff from gnu make for `make showbuilddir`
    Without this, we could get
    
    	make: Entering directory `<stem>'
    	<stem>/build/<arch>
    	make: Leaving directory `<stem>'
    
    instead of
    
    	<stem>/build/<arch>

--- scripts/gtkwave/disasm.sh
+++ scripts/gtkwave/disasm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 here=$(dirname $0)
-tas="$(make -C $here/../.. showbuilddir)/tas"
+tas="$(make -s -C $here/../.. showbuilddir)/tas"
 # We have to run a loop even in the error condition, to keep gtkwave from hanging
 [[ -x $tas ]] || { while read ; do echo "tas not found" ; done ; exit 1; }
 while true ; do
