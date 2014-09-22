2183b7438b4c9d6f2efa6baa538f7f8256932c47
Darren Kulp <darren@kulp.ch>
2014-09-21 23:05:46 -0500

    Improve GTKWave disassembly script

--- scripts/gtkwave/disasm.sh
+++ scripts/gtkwave/disasm.sh
@@ -4,6 +4,6 @@ tas="$(make -s -C $here/../.. showbuilddir)/tas"
 # We have to run a loop even in the error condition, to keep gtkwave from hanging
 [[ -x $tas ]] || { while read ; do echo "tas not found" ; done ; exit 1; }
 while true ; do
-	$tas --quiet --disassemble --format text - | tr -u -s ' ' | sed -le 's/^ //; s/0x0*/0x/'
+	$tas --strict --quiet --disassemble --format text - | tr -u -s ' ' | sed -le 's/^ //; s/0x0*\([0-9][0-9]*\)/0x\1/'
 	echo BAD
 done
