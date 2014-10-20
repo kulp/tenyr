kulp/tenyr

commit 8ca9f781de168bcd5796863b1deeac810ea78237

Add gzip dist target

Makefile
@@ -92,6 +92,13 @@ else
 .PHONY: all check coverage
 all: $(TARGETS)
 
+.PHONY: gzip
+gzip: tenyr-$(BUILD_NAME).tar.gz
+
+tenyr-$(BUILD_NAME).tar.gz: INSTALL_DIR := $(shell mktemp -d tenyr.gzip.XXXXXX)/tenyr-$(BUILD_NAME)
+tenyr-$(BUILD_NAME).tar.gz: local-install
+	tar zcf $@ -C $(INSTALL_DIR)/.. .
+
 clobber_FILES += $(BUILDDIR)/*.gc??
 coverage: CFLAGS  += --coverage
 coverage: LDFLAGS += --coverage
@@ -154,7 +161,7 @@ debugger_parser.h debugger_parser.c: debugger_lexer.h
 parser.h parser.c: lexer.h
 
 .PHONY: install local-install
-local-install: INSTALL_DIR = $(TOP)/dist/$(MACHINE)
+local-install: INSTALL_DIR ?= $(TOP)/dist/$(MACHINE)
 local-install: install
 
 install:: $(BIN_TARGETS)

commit 80ea4c5b8fbe4d59f477e563c7b139cedee0207f

Mend local-install rule broken by 8ca9f781

Makefile
@@ -97,11 +97,11 @@ gzip: tenyr-$(BUILD_NAME).tar.gz
 zip: tenyr-$(BUILD_NAME).zip
 
 tenyr-$(BUILD_NAME).tar.gz: INSTALL_DIR := $(shell mktemp -d tenyr.gzip.XXXXXX)/tenyr-$(BUILD_NAME)
-tenyr-$(BUILD_NAME).tar.gz: local-install
+tenyr-$(BUILD_NAME).tar.gz: install
 	tar zcf $@ -C $(INSTALL_DIR)/.. .
 
 tenyr-$(BUILD_NAME).zip: INSTALL_DIR := $(shell mktemp -d tenyr.zip.XXXXXX)/tenyr-$(BUILD_NAME)
-tenyr-$(BUILD_NAME).zip: local-install
+tenyr-$(BUILD_NAME).zip: install
 	orig=$(abspath $@) && (cd $(INSTALL_DIR)/.. ; zip -r $$orig .)
 
 clobber_FILES += $(BUILDDIR)/*.gc??
@@ -166,7 +166,7 @@ debugger_parser.h debugger_parser.c: debugger_lexer.h
 parser.h parser.c: lexer.h
 
 .PHONY: install local-install
-local-install: INSTALL_DIR ?= $(TOP)/dist/$(MACHINE)
+local-install: INSTALL_DIR = $(TOP)/dist/$(MACHINE)
 local-install: install
 
 install:: $(BIN_TARGETS)