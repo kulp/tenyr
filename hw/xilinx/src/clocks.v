b387e92345a932873630c58618149ea089ae103c
Darren Kulp <darren@kulp.ch>
2014-11-21 20:23:00 -0600

    Stabler reset behaviour
    Change DCM_SP to DCM_CLKGEN. In the process, reset behaviour improved
    stability : `make burn` with reset held down now comes out of reset
    properly, and bouncing on reset does not have obvious unpredictable
    results.

--- hw/xilinx/src/clocks.v
+++ hw/xilinx/src/clocks.v
@@ -1,32 +1,24 @@
 `timescale 1ns/1ps
 module tenyr_mainclock(input clk_in, reset, output clk_core, clk_vga);
 
-    wire clk_sys, clk_in_buf;
+    wire clk_sys, clk_in_buf, locked_core, locked_vga, clk_core_un, clk_vga_un;
 
-    BUFG BUFG_clk_in(.I(clk_in), .O(clk_in_buf));
+    BUFG   BUFG_clk_in  (.I(clk_in),      .O(clk_in_buf));
+    BUFGCE BUFG_clk_core(.I(clk_core_un), .O(clk_core  ), .CE(locked_core));
+    BUFGCE BUFG_clk_vga (.I(clk_vga_un ), .O(clk_vga   ), .CE(locked_vga ));
 
-    DCM_SP #(
-        .CLK_FEEDBACK       ( "1X"                 ),
-        .CLKDV_DIVIDE       ( 4.0                  ),
-        .CLKFX_DIVIDE       ( 5                    ),
-        .CLKFX_MULTIPLY     ( 4                    ),
-        .CLKIN_DIVIDE_BY_2  ( "FALSE"              ),
-        .CLKIN_PERIOD       ( 10.0                 ),
-        .CLKOUT_PHASE_SHIFT ( "NONE"               ),
-        .DESKEW_ADJUST      ( "SYSTEM_SYNCHRONOUS" ),
-        .PHASE_SHIFT        ( 0                    ),
-        .STARTUP_WAIT       ( "TRUE"               )
-    ) DCM_inst(
-        .CLK0     ( clk_sys    ),
-        .CLKDV    ( clk_vga    ),
-        .CLKFX    ( clk_core   ),
-        .CLKFB    ( clk_sys    ),
-        .CLKIN    ( clk_in_buf ),
-        .PSCLK    ( 1'b0       ),
-        .PSEN     ( 1'b0       ),
-        .PSINCDEC ( 1'b0       ),
-        .DSSEN    ( 1'b0       ),
-        .RST      ( reset      )
+    DCM_CLKGEN #(.CLKFX_DIVIDE(5), .CLKFX_MULTIPLY(4)) clk_gen_core(
+        .CLKIN  ( clk_in_buf  ),
+        .CLKFX  ( clk_core_un ),
+        .RST    ( reset       ),
+        .LOCKED ( locked_core )
+    );
+
+    DCM_CLKGEN #(.CLKFX_DIVIDE(8), .CLKFX_MULTIPLY(2)) clk_gen_vga(
+        .CLKIN  ( clk_in_buf ),
+        .CLKFX  ( clk_vga_un ),
+        .RST    ( reset      ),
+        .LOCKED ( locked_vga )
     );
 
 endmodule
