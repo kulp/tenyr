5a63be2811db5e12b24b3d932f9771cace715b35
Darren Kulp <darren@kulp.ch>
2015-01-19 12:20:14 -0600

    Mend trailz implementation
    Stop using <> operator and update implementation. Test incoming.

--- lib/trailz.tas.cpp
+++ lib/trailz.tas.cpp
@@ -7,31 +7,43 @@ trailz:
     B   <- C - 1
     B   <- C ^ B
     C   <- C & B
+    // now only one bit is set in C if C was nonzero
+    // if C was zero, C is still zero, and we exit early
+    D   <- C == 0
+    B   <- D & 32
+    jnzrel(D,done)
 
-    D   <- 0            // D is the building register, moved to B at the end
-    F   <- 0xff         // E,F are mask registers
-    E   <- F <<  8 + F  // E <- 0x0000ffff
-    F   <- F << 16 + F  // F <- 0x00ff00ff
+    // D is the building register, moved to B at the end
+    E   <- 0xffff       // E,F are mask registers
 
-    B   <- C & E        // B <- high 16 bits of C
-    B   <- B <> A + 1   // B <- (high 16 bits nonzero) ? 1 : 0
-    D   <- D << 1 + B   // D <- ((high 16 bits nonzero) ? 1 : 0) + (D << 1)
+    // if we find only zeros in one half, the other half has a bit set
+    B   <- C &~ E       // B <- 16 MSb of C
+    D   <- B == A + 1   // D <- (16 MSb zero) ? 0 : 1
 
-    B   <- C & F        // B <- high 8 bits of C
-    B   <- B <> A + 1   // B <- (high 8 bits nonzero) ? 1 : 0
-    D   <- D << 1 + B   // D <- ((high 8 bits nonzero) ? 1 : 0) + (D << 1)
+    F   <- E << 8       // F <- 0x00ffff00
+    F   <- E ^  F       // F <- 0x00ff00ff
+    B   <- C &~ F       // B <- 8 MSb of C
+    B   <- B == A + 1   // B <- (8 MSb zero) ? 0 : 1
+    D   <- D << 1 + B   // shift and add
 
-    B   <- C & 0x0f     // 00001111
-    B   <- B <> A + 1   // B <- (set bit is in bottom half) ? 0 : 1
+    E   <- F << 4       // E <- 0x0ff00ff0
+    E   <- F ^  E       // E <- 0x0f0f0f0f
+    B   <- C &~ E       // 00001111
+    B   <- B == A + 1   // B <- (4 MSb zero) ? 0 : 1
     B   <- D << 1 + B
 
-    D   <- C & 0x33     // 00110011
-    D   <- D <> A + 1
+    F   <- E << 2       // F <- 0x3c3c3c3c
+    F   <- E ^  F       // F <- 0x33333333
+    D   <- C &~ F
+    D   <- D == A + 1
     D   <- B << 1 + D
 
-    B   <- C & 0x55     // 01010101
-    B   <- B <> A + 1
+    E   <- F << 1       // E <- 0x66666666
+    E   <- F ^  E       // E <- 0x55555555
+    B   <- C &~ E
+    B   <- B == A + 1
     B   <- D << 1 + B
 
+done:
     popall_ret(D,E,F)
 
