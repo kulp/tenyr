26be584c332662a4a941a84c9da7dc633b1b1884
Darren Kulp <darren@kulp.ch>
2015-01-20 11:20:46 -0600

    Include `+` op test

---
+++ test/ops/add.tas.cpp
@@ -0,0 +1,121 @@
+C <- [(@args + (0 + LOAD_ADDRESS))]
+D <- [(@args + (1 + LOAD_ADDRESS))]
+
+// 16 unrolled repetitions of this block
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+E <- C ^ D
+D <- C & D
+D <- D << 1
+C <- E ^ D
+D <- E & D
+D <- D << 1
+
+B <- C + D
+B <- B == C
+illegal
+
+args: .word RAND0 ; .word RAND1 ; .word RAND2
