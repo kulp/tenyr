kulp/tenyr

commit dd57865cd41153d71c12e4cb6a074820980bae6d

Improve cleanliness of `clobber` rule

Makefile
@@ -87,6 +87,10 @@ clean clobber::
 clobber_FILES += $(BUILDDIR)/*.gc??
 clobber_FILES += $(BUILDDIR)/coverage.info
 clobber_FILES += $(BUILDDIR)/PERIODS.mk
+clobber_FILES += $(BUILDDIR)/coverage_html
+clobber_FILES += $(TOP)/test/op/*.texe
+clobber_FILES += $(TOP)/test/run/*.texe
+clobber_FILES += $(SOURCEFILES:.c=.d)
 clobber::
 	-$(MAKE) -C $(TOP)/ex $@
 
@@ -147,7 +151,6 @@ coverage.info.trimmed: coverage.info
 coverage.info.%: coverage.info.trimmed
 	lcov --extract $< '*/$*/*' --output-file $@
 
-CLOBBER_FILES += $(BUILDDIR)/coverage_html
 coverage_html_%: coverage.info.%
 	genhtml $< --output-directory $@
 

hw/icarus/Makefile
@@ -12,6 +12,7 @@ INCLUDES += $(INCLUDE_OS)
 
 clean_FILES = tenyr glue.vpi *.o
 clobber_FILES = *.vcd
+clobber_FILES = *.texe
 
 all: tenyr glue.vpi
 

commit e7adcbb61847194184572472ab1d273eba9fdba3

Improve clobber rule some more
A small regression was introduced in dd57865 due to a string being
overwritten instead of concatenated.

Now we clean up generated verilog files in hw/icarus/.

Now we properly clean up .d (dependency) files in the `clean` rule.

Now we clean up random files generated by `make dogfood`.

Makefile
@@ -51,12 +51,14 @@ SOURCEFILES = $(wildcard $(TOP)/src/*.c $(TOP)/src/devices/*.c)
 VPATH += $(TOP)/src $(TOP)/src/devices
 INCLUDES += $(TOP)/src $(INCLUDE_OS) $(BUILDDIR)
 
-clean_FILES := $(addprefix $(BUILDDIR)/, \
+clean_FILES = $(addprefix $(BUILDDIR)/,  \
                    *.o                   \
                    *.d                   \
                    parser.[ch]           \
                    lexer.[ch]            \
                    $(TARGETS)            \
+                   $(SOURCEFILES:$(TOP)/src/%.c=%.d) \
+                   random random.*       \
                )#
 
 tas_OBJECTS  = common.o asmif.o asm.o obj.o parser.o lexer.o
@@ -90,7 +92,6 @@ clobber_FILES += $(BUILDDIR)/PERIODS.mk
 clobber_FILES += $(BUILDDIR)/coverage_html
 clobber_FILES += $(TOP)/test/op/*.texe
 clobber_FILES += $(TOP)/test/run/*.texe
-clobber_FILES += $(SOURCEFILES:.c=.d)
 clobber::
 	-$(MAKE) -C $(TOP)/ex $@
 

hw/icarus/Makefile
@@ -11,8 +11,9 @@ CFLAGS += -I$(TOP)/src
 INCLUDES += $(INCLUDE_OS)
 
 clean_FILES = tenyr glue.vpi *.o
-clobber_FILES = *.vcd
-clobber_FILES = *.texe
+clobber_FILES  = *.vcd
+clobber_FILES += *.texe
+clobber_FILES += $(GEN_V)
 
 all: tenyr glue.vpi
 