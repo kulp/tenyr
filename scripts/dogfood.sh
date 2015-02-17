fc0b33d7ee87235f03e3a57fab7b89dc951340aa
Darren Kulp <darren@kulp.ch>
2015-02-17 11:37:36 -0600

    Check more object formats in dogfooding

--- scripts/dogfood.sh
+++ scripts/dogfood.sh
@@ -16,20 +16,21 @@ function fail ()
 	cp -p $file $base.{en,de}.? dogfood_failures/$bn/
 }
 
-fmt=text
-for file in $* ; do
-	trap "rm $base.{en,de}.[123]" EXIT
-	if [[ $file = *.cpp ]] ; then
-		pp="cpp -I$here/../lib"
-	else
-		pp=cat
-	fi
-	$pp $file | $tas -f $fmt - | tee $base.en.1 | $tas -d -f $fmt - | tee $base.de.1 | $tas -f $fmt - | tee $base.en.2 | $tas -d -f $fmt - | tee $base.de.2 | $tas -f $fmt -o $base.en.3 -
-	diff -q $base.en.1 $base.en.2 && diff -q $base.en.2 $base.en.3 && echo $(basename $file): OK || fail $file
-done
+# TODO support obj when identical objects can be made reliably
+for fmt in raw text ; do # TODO update when memh supports input
+	for file in $* ; do
+		trap "rm $base.{en,de}.[123]" EXIT
+		if [[ $file = *.cpp ]] ; then
+			pp="cpp -I$here/../lib"
+		else
+			pp=cat
+		fi
+		$pp $file | $tas -f $fmt - | tee $base.en.1 | $tas -d -f $fmt - | tee $base.de.1 | $tas -f $fmt - | tee $base.en.2 | $tas -d -f $fmt - | tee $base.de.2 | $tas -f $fmt -o $base.en.3 -
+		diff -q $base.en.1 $base.en.2 && diff -q $base.en.2 $base.en.3 && echo $(basename $file): OK || fail $file
+	done
 
-fmt=raw
-base=random
-$here/random.sh | tee $base.en.1 | $tas -d -f $fmt - | tee $base $base.de.1 | $tas -f $fmt - | tee $base.en.2 | $tas -d -f $fmt - | tee $base.de.2 | $tas -f $fmt -o $base.en.3 -
-diff -q $base.en.1 $base.en.2 && diff -q $base.en.2 $base.en.3 && echo $(basename $file): OK || fail $base
+	base=random
+	$here/random.sh | tee $base.en.1 | $tas -d -f $fmt - | tee $base $base.de.1 | $tas -f $fmt - | tee $base.en.2 | $tas -d -f $fmt - | tee $base.de.2 | $tas -f $fmt -o $base.en.3 -
+	diff -q $base.en.1 $base.en.2 && diff -q $base.en.2 $base.en.3 && echo $(basename $file): OK || fail $base
+done
 
