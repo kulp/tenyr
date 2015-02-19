e0b08439135d8f9fc1963dbf04c0bf43b1f4ac9b
Darren Kulp <darren@kulp.ch>
2015-02-19 08:18:20 -0600

    Generalise random dogfooding
    Show format in dogfood script output
    
    Refactoring dogfooding script
    
    Encode format into filename during dogfooding
    
    Emit verbose trace in dogfooding when V=1

--- scripts/dogfood.sh
+++ scripts/dogfood.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Usage: dogfood.sh tempfile-stem path-to-tas file.tas[.cpp] [ files... ]
+if [[ $V == 1 ]] ; then set -x ; fi
 here=`dirname $BASH_SOURCE`
 stem=`basename $1`
 tas=$2
@@ -9,28 +10,65 @@ base=`mktemp -t $stem`
 
 function fail ()
 {
-	file=$1
+	fmt=$1
+	file=$2
 	bn=$(basename $file)
 	echo $bn: FAILED
 	mkdir -p dogfood_failures/$bn
-	cp -p $file $base.{en,de}.? dogfood_failures/$bn/
+	cp -p $file $base.$fmt.{en,de}.? dogfood_failures/$bn/
+}
+
+function en ()
+{
+	fmt=$1
+	instance=$2
+	$tas -f $fmt - | tee $base.$fmt.en.$instance
+}
+
+function de ()
+{
+	fmt=$1
+	instance=$2
+	$tas -d -f $fmt - | tee $base.$fmt.de.$instance
+}
+
+function check ()
+{
+	fmt=$1
+	typ=$2
+	diff -q $base.$fmt.$typ.$3 $base.$fmt.$typ.$4
+}
+
+function cycle ()
+{
+	fmt=$1
+	en $fmt 1 | de $fmt 1 | en $fmt 2 | de $fmt 2 | en $fmt 3 | de $fmt 3 > /dev/null
+}
+
+function match ()
+{
+	fmt=$1
+	typ=$2
+	file=$3
+	check $fmt $typ 1 2 && check $fmt $typ 2 3 && echo $(basename $file) @ $fmt: OK || fail $fmt $file
 }
 
 # TODO support obj when identical objects can be made reliably
 for fmt in raw text ; do # TODO update when memh supports input
 	for file in $* ; do
-		trap "rm $base.{en,de}.[123]" EXIT
+		trap "rm $base.$fmt.{en,de}.[0123]" EXIT
 		if [[ $file = *.cpp ]] ; then
 			pp="cpp -I$here/../lib"
 		else
 			pp=cat
 		fi
-		$pp $file | $tas -f $fmt - | tee $base.en.1 | $tas -d -f $fmt - | tee $base.de.1 | $tas -f $fmt - | tee $base.en.2 | $tas -d -f $fmt - | tee $base.de.2 | $tas -f $fmt -o $base.en.3 -
-		diff -q $base.en.1 $base.en.2 && diff -q $base.en.2 $base.en.3 && echo $(basename $file): OK || fail $file
+		$pp $file | cycle $fmt
+		match $fmt en $file
 	done
 
 	base=random
-	$here/random.sh | tee $base.en.1 | $tas -d -f $fmt - | tee $base $base.de.1 | $tas -f $fmt - | tee $base.en.2 | $tas -d -f $fmt - | tee $base.de.2 | $tas -f $fmt -o $base.en.3 -
-	diff -q $base.en.1 $base.en.2 && diff -q $base.en.2 $base.en.3 && echo $(basename $file): OK || fail $base
+	file=$base
+	$here/random.sh | tee $base | de text 0 | cycle $fmt
+	match $fmt en $file
 done
 
