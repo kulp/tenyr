cf080d7205bab1a3d23a107b85b9f14b557cc688
Darren Kulp <darren@kulp.ch>
2015-07-21 07:36:02 -0500

    Unify whitespace in dogfood.sh

--- scripts/dogfood.sh
+++ scripts/dogfood.sh
@@ -13,51 +13,51 @@ fail_fast=${FAIL_FAST:-1}
 
 function fail ()
 {
-	fmt=$1
-	file=$2
-	bn=$(basename $file)
-	echo $bn: FAILED
-	mkdir -p dogfood_failures/$bn
-	cp -p $file $base.$fmt.{en,de}.*.? dogfood_failures/$bn/
-	rc=1
-	if [[ $fail_fast = 1 ]] ; then exit $rc ; fi
+    fmt=$1
+    file=$2
+    bn=$(basename $file)
+    echo $bn: FAILED
+    mkdir -p dogfood_failures/$bn
+    cp -p $file $base.$fmt.{en,de}.*.? dogfood_failures/$bn/
+    rc=1
+    if [[ $fail_fast = 1 ]] ; then exit $rc ; fi
 }
 
 function en ()
 {
-	fmt=$1
-	instance=$2
-	$tas $flags -f $fmt - | tee $base.$fmt.en.$flags.$instance
-	if [[ $? != 0 ]] ; then return 1 ; fi
+    fmt=$1
+    instance=$2
+    $tas $flags -f $fmt - | tee $base.$fmt.en.$flags.$instance
+    if [[ $? != 0 ]] ; then return 1 ; fi
 }
 
 function de ()
 {
-	fmt=$1
-	instance=$2
-	$tas $flags -d -f $fmt - | tee $base.$fmt.de.$flags.$instance
-	if [[ $? != 0 ]] ; then return 1; fi
+    fmt=$1
+    instance=$2
+    $tas $flags -d -f $fmt - | tee $base.$fmt.de.$flags.$instance
+    if [[ $? != 0 ]] ; then return 1; fi
 }
 
 function check ()
 {
-	fmt=$1
-	typ=$2
-	diff -q $base.$fmt.$typ.$flags.$3 $base.$fmt.$typ.$flags.$4
+    fmt=$1
+    typ=$2
+    diff -q $base.$fmt.$typ.$flags.$3 $base.$fmt.$typ.$flags.$4
 }
 
 function cycle ()
 {
-	fmt=$1
-	en $fmt 1 | de $fmt 1 | en $fmt 2 | de $fmt 2 | en $fmt 3 | de $fmt 3 > /dev/null
-	if [[ $? != 0 ]] ; then return 1 ; fi
+    fmt=$1
+    en $fmt 1 | de $fmt 1 | en $fmt 2 | de $fmt 2 | en $fmt 3 | de $fmt 3 > /dev/null
+    if [[ $? != 0 ]] ; then return 1 ; fi
 }
 
 function match ()
 {
-	fmt=$1
-	typ=$2
-	file=$3
+    fmt=$1
+    typ=$2
+    file=$3
     check $fmt $typ 1 2 && check $fmt $typ 2 3 && $ECHO "$(basename $file) @ $fmt ($flags) : OK" || fail $fmt $file
 }
 
