ff22a65af00da4c78adfc504af67db2b86be43df
Darren Kulp <darren@kulp.ch>
2015-08-04 07:35:57 -0500

    Comment MMR design

--- hw/verilog/mmr.v
+++ hw/verilog/mmr.v
@@ -4,13 +4,13 @@
 module mmr(input clk, strobe, rw, re, we, output[PBITS-1:0] d_out,
            input[ABITS-1:0] addr, input[PBITS-1:0] d_in, inout[DBITS-1:0] val);
 
-    parameter ADDR    = 0;
-    parameter RE      = 1; // reads possible ?
-    parameter WE      = 1; // writes possible ?
-    parameter DEFAULT = 0;
-    parameter ABITS   = 32;
-    parameter PBITS   = 32;
-    parameter DBITS   = PBITS;
+    parameter ADDR    = 0;      // self address
+    parameter RE      = 1;      // reads possible ?
+    parameter WE      = 1;      // writes possible ?
+    parameter DEFAULT = 0;      // default value
+    parameter ABITS   = 32;     // bits of address
+    parameter PBITS   = 32;     // bits of data ports
+    parameter DBITS   = PBITS;  // bits of value port
 
     reg[DBITS - 1:0] store = DEFAULT;
 
