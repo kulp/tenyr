kulp/tenyr

commit 2693d72b336e2b468d2e353ceb409c6feedf5f03

Inline debugwrap device
There's no reason not to pretend this behaviour is a full-fledged
device. Removing it also stops a memory leak.

src/simif.c
@@ -42,6 +42,12 @@ int dispatch_op(void *ud, int op, uint32_t addr, uint32_t *data)
         fprintf(stderr, "No device handles address %#x\n", addr);
         return -1;
     }
+
+    if (s->conf.verbose > 2) {
+        printf("%-5s @ 0x%08x = 0x%08x\n",
+               (op == OP_WRITE) ? "write" : "read", addr, *data);
+    }
+
     // TODO don't send in the whole simulator state ? the op should have
     // access to some state, in order to redispatch and potentially use other
     // machine.devices, but it shouldn't see the whole state
@@ -70,12 +76,6 @@ int devices_setup(struct sim_state *s)
 
 int devices_finalise(struct sim_state *s)
 {
-    if (s->conf.verbose > 2) {
-        assert(("device to be wrapped is not NULL", s->machine.devices[0] != NULL));
-        int debugwrap_wrap_device(struct device **device);
-        debugwrap_wrap_device(&s->machine.devices[0]);
-    }
-
     // Devices must be in address order to allow later bsearch. Assume they do
     // not overlap (overlap is illegal).
     qsort(s->machine.devices, s->machine.devices_count,

mk/common.mk
@@ -102,7 +102,7 @@ BUILD_NAME := $(shell $(GIT) describe --always --tags --match 'v?.?.?*' 2>/dev/n
 TAS = $(runwrap)$(TOOLDIR)/tas$(EXE_SUFFIX)
 TLD = $(runwrap)$(TOOLDIR)/tld$(EXE_SUFFIX)
 
-DEVICES = ram sparseram debugwrap serial spi
+DEVICES = ram sparseram serial spi
 DEVOBJS = $(DEVICES:%=%.o)
 # plugin devices
 PDEVICES += spidummy spisd spi

src/devices/debugwrap.c
@@ -1,69 +1,0 @@
-#include <assert.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-#include "common.h"
-#include "device.h"
-#include "sim.h"
-
-struct debugwrap_state {
-    struct device *wrapped;
-};
-
-static int debugwrap_init(struct plugin_cookie *pcookie, void *cookie)
-{
-    // our own init done in debugwrap_add_device ()
-    struct debugwrap_state *debugwrap = *(void**)cookie;
-    debugwrap->wrapped->ops.init(pcookie, &debugwrap->wrapped->cookie);
-    return 0;
-}
-
-static int debugwrap_fini(void *cookie)
-{
-    struct debugwrap_state *debugwrap = cookie;
-    debugwrap->wrapped->ops.fini(debugwrap->wrapped->cookie);
-    free(debugwrap);
-
-    return 0;
-}
-
-static int debugwrap_op(void *cookie, int op, uint32_t addr,
-        uint32_t *data)
-{
-    struct debugwrap_state *debugwrap = cookie;
-
-    debugwrap->wrapped->ops.op(debugwrap->wrapped->cookie, op, addr, data);
-    printf("%-5s @ 0x%08x = 0x%08x\n", (op == OP_WRITE) ? "write" : "read", addr, *data);
-
-    return 0;
-}
-
-int debugwrap_add_device(struct device **device, struct device *wrap)
-{
-    struct debugwrap_state *debugwrap = malloc(sizeof *debugwrap);
-    *debugwrap = (struct debugwrap_state){ .wrapped = wrap };
-
-    **device = (struct device){
-        .bounds = { wrap->bounds[0], wrap->bounds[1] },
-        .ops = {
-            .op = debugwrap_op,
-            .init = debugwrap_init,
-            .fini = debugwrap_fini,
-        },
-        .cookie = debugwrap,
-    };
-
-    return 0;
-}
-
-int debugwrap_wrap_device(struct device **device)
-{
-    struct device *copy = malloc(sizeof *copy);
-    *copy = **device;
-
-    return debugwrap_add_device(device, copy);
-}
-
-/* vi: set ts=4 sw=4 et: */

ui/web/Makefile
@@ -111,14 +111,14 @@ tcc.bc:
 tas$(EXE_SUFFIX) tsim$(EXE_SUFFIX): common.o
 tas$(EXE_SUFFIX) tld$(EXE_SUFFIX): emtsearch.o emlsearch.o
 tas$(EXE_SUFFIX): asm.o asmif.o param.o parser.o lexer.o obj.o
-tsim$(EXE_SUFFIX): sim.o simif.o param.o asm.o obj.o ram.o serial.o debugwrap.o emlsearch.o
+tsim$(EXE_SUFFIX): sim.o simif.o param.o asm.o obj.o ram.o serial.o emlsearch.o
 tld$(EXE_SUFFIX): common.o obj.o
 
 tsim.js: CC_OPT = -O2 # -O0 causes failure
 # --closure 1 doesn't work with tcc.js yet - reason not known.
 tcc.js: EMCCFLAGS_LD += --closure 0
 
-asm.o debugwrap.o ram.o serial.o sparseram.o parser.o lexer.o: CFLAGS += -Wno-unused-parameter
+asm.o ram.o serial.o sparseram.o parser.o lexer.o: CFLAGS += -Wno-unused-parameter
 asm.o simif.o: CFLAGS += -Wno-unused-value
 parser.o lexer.o: CFLAGS += -Wno-unused -Wno-sign-compare
 lexer.o: CFLAGS += -Wno-unneeded-internal-declaration

commit 8c080e1bf60ac425e479ca39a5eef38e8c0ca5c1

Mend broken debugging from 2693d72
The printing of the data read was happening before the read itself.

src/simif.c
@@ -43,15 +43,17 @@ int dispatch_op(void *ud, int op, uint32_t addr, uint32_t *data)
         return -1;
     }
 
+    // TODO don't send in the whole simulator state ? the op should have
+    // access to some state, in order to redispatch and potentially use other
+    // machine.devices, but it shouldn't see the whole state
+    int result = (*device)->ops.op((*device)->cookie, op, addr, data);
+
     if (s->conf.verbose > 2) {
         printf("%-5s @ 0x%08x = 0x%08x\n",
                (op == OP_WRITE) ? "write" : "read", addr, *data);
     }
 
-    // TODO don't send in the whole simulator state ? the op should have
-    // access to some state, in order to redispatch and potentially use other
-    // machine.devices, but it shouldn't see the whole state
-    return (*device)->ops.op((*device)->cookie, op, addr, data);
+    return result;
 }
 
 static int compare_devices_by_base(const void *_a, const void *_b)