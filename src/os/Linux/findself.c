344aae040500da1d24b92d3b50954deab4b24cf6
Darren Kulp <darren@kulp.ch>
2016-05-14 15:15:16 -0700

    Generalise and robustify findself on Linux

--- src/os/Linux/findself.c
+++ src/os/Linux/findself.c
@@ -5,15 +5,14 @@
 char *os_find_self(void)
 {
     // PATH_MAX (used by readlink(2)) is not necessarily available
-    size_t size = 4096;
-    char *path = malloc(size);
-    size_t used = readlink("/proc/self/exe", path, size);
-    // need strictly less than size, or else we probably truncated
-    if (used < size) {
-        return path;
-    } else {
-        free(path);
-        return NULL;
-    }
+    size_t size = 2048, used = 0;
+    char *path = NULL;
+    do {
+        size *= 2;
+        path = realloc(path, size);
+        used = readlink("/proc/self/exe", path, size);
+    } while (used > size && path != NULL);
+
+    return path;
 }
 
