e2119c9b991c44e8cc264d50c6ecbe31c26fbde2
Darren Kulp <darren@kulp.ch>
2016-05-14 14:47:56 -0700

    Stop warning about strict prototyping rules
    We want a pseudo-prototype that doesn't specify arguments or lack
    thereof.

--- mk/rules.mk
+++ mk/rules.mk
@@ -52,8 +52,8 @@ libtenyr%$(DYLIB_SUFFIX): %,dy.o
 	@$(MAKESTEP) "[ DYLD ] $@"
 	$(LINK.c) -shared -o $@ $^ $(LDLIBS)
 
-%.vpi: CFLAGS  += -Wall -Wextra -Wshadow -pedantic-errors -std=c99
 %.vpi: CFLAGS  += $(shell $(IVERILOG)iverilog-vpi --cflags 2> /dev/null | sed s/-no-cpp-precomp//)
+%.vpi: CFLAGS  += -Wno-strict-prototypes
 # don't complain about unused values that we might use in asserts
 # it's all right for callbacks not to use all their parameters
 %.vpi: CFLAGS  += -Wno-unused-value -Wno-unused-parameter
