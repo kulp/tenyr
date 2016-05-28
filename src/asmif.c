kulp/tenyr

commit 8ad6d60762c8b69723a75b7fbe42b66cc65607c8

Stop using UB (left shift of negative number)

src/asmif.c
@@ -248,7 +248,7 @@ static int fixup_deferred_exprs(struct parse_data *pd)
                 rc |= 1;
             }
 
-            uint32_t mask = -1ll << r->width;
+            uint32_t mask = ~((1L << r->width) - 1);
             *r->dest &= mask;
             *r->dest |= result & ~mask;
             ce_free(ce);

src/common.h
@@ -14,7 +14,7 @@
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 #define MSB32_(Bits,X) ((1ULL << ((Bits) - 1)) & (uint32_t)(X))
 #define EXTEND32_(Bits,X) ((-!!MSB32_(Bits,X)) << (Bits))
-#define SEXTEND32(Bits,X) ((Bits) >= 32 ? (uint32_t)(X) : (uint32_t)(EXTEND32_(Bits,X) | (((uint32_t)X) & ~(-1UL << (Bits)))))
+#define SEXTEND32(Bits,X) ((Bits) >= 32 ? (uint32_t)(X) : (uint32_t)(EXTEND32_(Bits,X) | (((uint32_t)X) & ((1UL << (Bits)) - 1))))
 
 #define UNUSED   __attribute__((unused))
 #define NORETURN __attribute__((noreturn))

src/sim.c
@@ -27,7 +27,7 @@ static void do_op(enum op op, int type, int32_t *rhs, uint32_t X, uint32_t Y,
     #define Ps(x) ((( int32_t*)p)[x])
     #define Pu(x) (((uint32_t*)p)[x])
     uint32_t pack2 = Pu(2) << 12;
-    uint32_t pack1 = Pu(1) & ~(-1UL << 12);
+    uint32_t pack1 = Pu(1) & 0xfff;
      int32_t pack0 = Ps(0);
 
     switch (op) {

commit f6fc87312f0cc715095162ae8a4217c2dbdfc3ee

Mend breakage of MinGW build from 8ad6d60
Different type sizes on Windows caused a shift to change meaning for the
MinGW build in 8ad6d60. This change mends that failure.

src/asmif.c
@@ -239,7 +239,7 @@ static int fixup_deferred_exprs(struct parse_data *pd)
                 rc |= 1;
             }
 
-            uint32_t mask = ~((1L << r->width) - 1);
+            uint32_t mask = ~((1LL << r->width) - 1);
             *r->dest &= mask;
             *r->dest |= result & ~mask;
             ce_free(ce);