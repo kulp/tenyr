1aeaf4db7a28fcb99271edc39ff78d3a0464fd26
Darren Kulp <darren@kulp.ch>
2016-05-30 12:05:16 -0700

    Use new emscripten MODULARIZE functionality

--- ui/web/pre_wrap.js
+++
@@ -1,9 +0,0 @@
-var tenyr_state; if (!tenyr_state) tenyr_state = {};
-(function(prefix, tenyr_state){
-var ENVIRONMENT_IS_NODE = typeof process === 'object';
-var ENVIRONMENT_IS_WEB = typeof window === 'object';
-var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
-var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
-
-//if (typeof tenyr_state == 'undefined') { tenyr_state = { } }
-(function(Module){
