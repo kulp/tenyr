026705445fa69922f53e5e6e2de72c5ab7fff045
Darren Kulp <darren@kulp.ch>
2016-06-04 09:29:44 -0700

    Make tcc and tas run in demo page

--- ui/web/general_post.js
+++ ui/web/general_post.js
@@ -1,3 +1,21 @@
+tenyr_state.tcc.args = ['-n', '-rprealloc', '-rserial', '-ftext', '-'];
+tenyr_state.tcc.app = Module_tcc({
+    'thisProgram':'tcc',
+    'stdin':tenyr_state.tcc_get_line_char,
+    'stdout':function(ch) {
+        tenyr_state.tcc_out_area.value += String.fromCharCode(ch);
+    }
+});
+
+tenyr_state.tas.args = ['-ftext', '-'];
+tenyr_state.tas.app = Module_tas({
+    'thisProgram':'tas',
+    'stdin':tenyr_state.tas_get_line_char,
+    'stdout':function(ch) {
+        tenyr_state.tas_out_area.value += String.fromCharCode(ch);
+    }
+});
+
 tenyr_state.tsim.args = ['-n', '-rprealloc', '-rserial', '-ftext', '-'];
 tenyr_state.tsim.app = Module_tsim({
     'thisProgram':'tsim',
