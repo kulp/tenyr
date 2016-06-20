kulp/tenyr

commit 575915ca85111bf1cedfa2945f5f2c23c8125d84

Separate rules from vars in OS-specific makefiles

mk/common.mk
@@ -104,8 +104,8 @@ RESOURCES   := $(wildcard $(TOP)/rsrc/64/*.png) \
                $(wildcard $(TOP)/plugins/*.rcp) \
                #
 
-include $(TOP)/mk/os/default.mk
--include $(TOP)/mk/os/$(OS).mk
+include $(TOP)/mk/os/vars/default.mk
+-include $(TOP)/mk/os/vars/$(OS).mk
 include $(TOP)/mk/sdl.mk
 include $(TOP)/mk/jit.mk
 

Makefile
@@ -75,6 +75,8 @@ else
 
 all: $(TARGETS)
 
+-include $(TOP)/mk/os/rules/$(OS).mk
+
 .PHONY: vpi
 vpi: vpidevices.vpi
 vpidevices.vpi: callbacks,dy.o vpiserial,dy.o load,dy.o sim,dy.o asm,dy.o obj,dy.o common,dy.o param,dy.o

mk/os/emscripten.mk
@@ -1,76 +1,0 @@
-# this file is included by the main Makefile automatically
-export EXE_SUFFIX = .bc
-CFLAGS_PIC   = -s SIDE_MODULE=1
-LDFLAGS_PIC  = -s SIDE_MODULE=1
-EMCCFLAGS_LD = -s SIDE_MODULE=1
-CPPFLAGS += -DEMSCRIPTEN
-EMCC = emcc
-export CC := $(EMCC)
-JIT = 0
-
-USE_OWN_SEARCH = 1
-EMCCFLAGS_LD += -s MODULARIZE=1
-
-BUILDDIR = $(TOP)/ui/web/build
-
-BIN_TARGETS := $(BIN_TARGETS:$(EXE_SUFFIX)=.js) tcc.js
-
-SDL_OPTS = \
-	-s USE_SDL=2 \
-	-s USE_SDL_IMAGE=2 \
-	-s 'SDL2_IMAGE_FORMATS=["png"]' \
-	#
-
-ifeq ($(DEBUG),)
-CC_OPT = -O2
-CC_DEBUG =
-CLOSURE_FLAGS = --closure 1
-%.js: EMCCFLAGS_LD += -s ASSERTIONS=0 \
-                      -s LIBRARY_DEBUG=0 \
-                      -s DISABLE_EXCEPTION_CATCHING=1 \
-                      -s WARN_ON_UNDEFINED_SYMBOLS=0 \
-                      #
-else
-CC_OPT = -O0
-CC_DEBUG = -g
-CLOSURE_FLAGS = --closure 0
-%.js: EMCCFLAGS_LD += -s ASSERTIONS=2 \
-                      -s LIBRARY_DEBUG=0 \
-                      #
-endif
-%.js: EMCCFLAGS_LD += $(CC_OPT) $(CC_DEBUG)
-%.js: EMCCFLAGS_LD += $(CLOSURE_FLAGS)
-
-%.js: EMCCFLAGS_LD += -s "EXPORT_NAME='Module_$*'"
-%.js: EMCCFLAGS_LD += -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1
-
-%.js: %.bc
-	@$(MAKESTEP) "[ EM-LD ] $@"
-	$(EMCC) $(EMCCFLAGS_LD) $< $(LDLIBS) -o $@
-
-# Disable closure compiler for now
-tcc.js tas.js tsim.js tld.js: CLOSURE_FLAGS :=# empty
-
-# Disable closing of streams so that the same code can run again
-tas.bc tsim.bc tld.bc: CPPFLAGS += '-Dfclose=fflush'
-
-PP_BUILD = $(TOP)/3rdparty/tinypp
-tcc.o: libtcc.c tccpp.c tccgen.c tcc.h libtcc.h tcctok.h
-tcc.bc: CFLAGS := $(CC_DEBUG) -Wall
-tcc.bc: CFLAGS += -Wno-pointer-sign -Wno-sign-compare -fno-strict-aliasing -Wno-shift-negative-value
-tcc.bc: CFLAGS += $(CC_OPT)
-vpath %.c $(PP_BUILD)
-vpath %.h $(PP_BUILD)
-
-TENYR_LIB_DIR ?= $(TOP)/lib
-TH_FILES = $(wildcard $(TENYR_LIB_DIR)/*.th)
-TH_FLAGS = $(addprefix --preload-file ,$(foreach m,$(TH_FILES),$m@$(notdir $m)))
-tcc.js: EMCCFLAGS_LD += $(TH_FLAGS)
-
-RSRC_FILES = $(wildcard $(TOP)/rsrc/*.png)
-RSRC_FLAGS = $(addprefix --preload-file ,$(foreach m,$(RSRC_FILES),$m@rsrc/$(notdir $m)))
-tsim.js: $(RSRC_FILES)
-tsim.js: EMCCFLAGS_LD += $(RSRC_FLAGS)
-
-clean_FILES += $(BUILDDIR)/*.bc $(BUILDDIR)/*.js.mem $(BUILDDIR)/*.js $(BUILDDIR)/*.data
-

mk/os/rules/emscripten.mk
@@ -1,0 +1,21 @@
+tcc.js: EMCCFLAGS_LD += $(TH_FLAGS)
+
+tsim.js: $(RSRC_FILES)
+tsim.js: EMCCFLAGS_LD += $(RSRC_FLAGS)
+
+vpath %.c $(PP_BUILD)
+vpath %.h $(PP_BUILD)
+tcc.o: libtcc.c tccpp.c tccgen.c tcc.h libtcc.h tcctok.h
+tcc.bc: CFLAGS := $(CC_DEBUG) -Wall
+tcc.bc: CFLAGS += -Wno-pointer-sign -Wno-sign-compare -fno-strict-aliasing -Wno-shift-negative-value
+tcc.bc: CFLAGS += $(CC_OPT)
+
+%.js: %.bc
+	@$(MAKESTEP) "[ EM-LD ] $@"
+	$(EMCC) $(EMCCFLAGS_LD) $< $(LDLIBS) -o $@
+
+# Disable closure compiler for now
+tcc.js tas.js tsim.js tld.js: CLOSURE_FLAGS :=# empty
+
+# Disable closing of streams so that the same code can run again
+tas.bc tsim.bc tld.bc: CPPFLAGS += '-Dfclose=fflush'

commit 38eb25fa05c4f2eaf5e60673731e24f8f6bc458e

Mend breakage on Win32 build since 575915c

mk/common.mk
@@ -78,8 +78,6 @@ CPPFLAGS += $(patsubst %,-D%,$(DEFINES)) \
             $(patsubst %,-I%,$(INCLUDES))
 
 GIT = git --git-dir=$(TOP)/.git
-CC  := $(CROSS_COMPILE)$(CC)
-CXX := $(CROSS_COMPILE)$(CXX)
 FLEX  ?= flex
 BISON ?= bison -Werror
 
@@ -106,6 +104,12 @@ RESOURCES   := $(wildcard $(TOP)/rsrc/64/*.png) \
 
 include $(TOP)/mk/os/vars/default.mk
 -include $(TOP)/mk/os/vars/$(OS).mk
+
+# OS Makefiles might have set CROSS_COMPILE. Since we are using `:=` instead of
+# `=`, order of assignment of variables matters.
+CC  := $(CROSS_COMPILE)$(CC)
+CXX := $(CROSS_COMPILE)$(CXX)
+
 include $(TOP)/mk/sdl.mk
 include $(TOP)/mk/jit.mk
 