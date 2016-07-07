f86ca0e3414f76fdb7aaa54f966eb424e6db412f
Darren Kulp <darren@kulp.ch>
2016-07-07 11:16:58 -0700

    Add null check after param_get

--- src/param.c
+++ src/param.c
@@ -37,7 +37,7 @@ int param_get_int(struct param_state *pstate, const char *key, int *val)
     const char *str = NULL;
     char *next = NULL;
     int test = 0;
-    if (param_get(pstate, key, 1, (const void **)&str))
+    if (param_get(pstate, key, 1, (const void **)&str) && str != NULL)
         test = strtol(str, &next, 0);
 
     int good = next > str;
