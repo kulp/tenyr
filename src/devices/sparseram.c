6a8efcee0b39cf6fc54de5ce316ed029acf2a964
Darren Kulp <darren@kulp.ch>
2017-01-22 19:23:08 -0800

    Allocate in exact runtime page size
    Initialize sparseram pages with memset

--- src/devices/sparseram.c
+++ src/devices/sparseram.c
@@ -3,6 +3,7 @@
 #include <search.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "common.h"
 #include "device.h"
@@ -13,18 +14,16 @@
 // fact that it is nearly a page size is basically useless since it does not
 // fit evenly into pages). Consider allocating header ram_elements separately from
 // storage ram_elements and packing nicely into pages.
-// TODO make sensitive to run-time page size ?
-#define PAGESIZE    4096
-#define PAGEWORDS   (PAGESIZE / sizeof(uint32_t))
-#define WORDMASK    ((1 << 10) - 1)
 
 struct sparseram_state {
     void *mem;
+    long pagesize;
+    uint32_t mask;
 };
 
 struct ram_element {
     int32_t base;
-    uint32_t space[];
+    uint32_t *space;
 };
 
 static int tree_compare(const void *_a, const void *_b)
@@ -38,6 +37,9 @@ static int sparseram_init(struct plugin_cookie *pcookie, void *cookie)
 {
     struct sparseram_state *sparseram = *(void**)cookie = malloc(sizeof *sparseram);
     sparseram->mem = NULL;
+    sparseram->pagesize = sysconf(_SC_PAGESIZE);
+    // `mask` has only the bits set that address *within* a page
+    sparseram->mask = sparseram->pagesize / sizeof(uint32_t) - 1;
 
     return 0;
 }
@@ -48,6 +50,7 @@ static int sparseram_fini(void *cookie)
     while (sparseram->mem) {
         struct ram_element **np = sparseram->mem, *p  = *np;
         tdelete(p, &sparseram->mem, tree_compare);
+        free(p->space);
         free(p);
     }
     free(sparseram);
@@ -60,22 +63,24 @@ static int sparseram_op(void *cookie, int op, uint32_t addr,
 {
     struct sparseram_state *sparseram = cookie;
 
-    struct ram_element key = (struct ram_element){ addr & ~WORDMASK };
+    uint32_t page_base = addr & ~sparseram->mask;
+    struct ram_element key = (struct ram_element){ page_base, NULL };
     struct ram_element **p = tsearch(&key, &sparseram->mem, tree_compare);
     if (*p == &key) {
         // Currently, a page is allocated even on a read. It is not a very
         // useful optimisation, but we could avoid allocating a page until the
         // first write.
-        struct ram_element *node = malloc(PAGESIZE * sizeof *node->space + sizeof *node);
-        for (unsigned long i = 0; i < PAGESIZE; i++)
-            node->space[i] = 0xffffffff; // "illegal" ; will trap
+        struct ram_element *node = malloc(sizeof *node);
+        node->base  = page_base;
+        node->space = malloc(sparseram->pagesize);
+        // set all 32-bit words to 0xffffffff, the trap instruction
+        memset(node->space, 0xff, sparseram->pagesize);
 
-        *node = (struct ram_element){ addr & ~WORDMASK };
         *p = node;
     }
 
     assert(("Sparse page address is non-NULL", *p != NULL));
-    uint32_t *where = &(*p)->space[addr & WORDMASK];
+    uint32_t *where = &(*p)->space[addr & sparseram->mask];
 
     if (op == OP_WRITE)
         *where = *data;
