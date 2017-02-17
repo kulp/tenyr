b7daf9451402f0806e237b9b2804593e6fac266e
Darren Kulp <darren@kulp.ch>
2017-02-17 16:51:15 -0700

    Add new os_set_non_blocking()

--- src/os/default/open.c
+++ src/os/default/open.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <fcntl.h>
 
 FILE *os_fopen(const char *path, const char *mode)
 {
     return fopen(path, mode);
+}
+
+int os_set_non_blocking(FILE *stream)
+{
+    int fd = fileno(stream);
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK))
+        return 1;
+
+    return 0;
 }
 
--- src/os/default/os_common.h
+++ src/os/default/os_common.h
@@ -19,6 +19,7 @@ int os_get_tsimrc_path(char buf[], size_t sz);
 long os_getpagesize();
 int os_preamble();
 int os_set_buffering(FILE *stream, int mode);
+int os_set_non_blocking(FILE *stream);
 
 #endif
 
