4e481f4bd6a8415bd43af853398770f5d2aaa639
Darren Kulp <darren@kulp.ch>
2017-03-08 13:34:20 -0700

    Add baremetal display of character set

---
+++ ex/bm_chars.tas.cpp
@@ -0,0 +1,29 @@
+#include "common.th"
+#include "vga.th"
+
+_start:
+    prologue
+    b <- 0              // indicate non-completion to testbench
+
+    call(init_display)
+    call(disable_cursor)
+
+restart:
+    c <- 0
+    j <- @VGA_BASE
+    k <- (ROWS * COLS)
+    k <- k + j
+
+top:
+    c -> [j]
+    c <- c + 1
+    c <- c & 0xff
+
+    j <- j + 1
+    n <- j < k
+    jnzrel(n,top)
+
+    b <- -1             // indicate completion to testbench
+    goto(restart)     // restartable, but exits to testbench by default
+    illegal
+
