59a02d9d0232ed21cddd04d2c60131e9e3e6e69f
Darren Kulp <darren@kulp.ch>
2017-03-24 06:38:46 -0700

    Add no-op Win32 impl for non-blocking fd

---
+++ src/os/Win32/open.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+FILE *os_fopen(const char *path, const char *mode)
+{
+    return fopen(path, mode);
+}
+
+int os_set_non_blocking(FILE *stream)
+{
+    (void)stream;
+    /* TODO support non-blocking (overlapped ?) file handles */
+    return 1; // return failure for now
+}
+
