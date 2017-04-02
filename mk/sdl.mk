bfd184c2ca3dc125b62196ab014db6247d87dc30
Darren Kulp <darren@kulp.ch>
2017-04-01 20:42:22 -0400

    Make libtenyrsdlvga build font file

--- mk/sdl.mk
+++ mk/sdl.mk
@@ -7,6 +7,9 @@ PDEVICES += $(PDEVICES_SDL)
 libtenyrsdl%$(DYLIB_SUFFIX) $(PDEVICES_SDL:%=devices/%.d): \
     CPPFLAGS += $(call sdl2_pkg_config,--cflags,sdl2 SDL2_image)
 libtenyrsdl%$(DYLIB_SUFFIX): LDLIBS += $(call sdl2_pkg_config,--libs,sdl2 SDL2_image)
+libtenyrsdlvga$(DYLIB_SUFFIX): | $(TOP)rsrc/font10x15/invert.font10x15.png
+$(TOP)rsrc/font10x15/%:
+	$(MAKE) -C $(@D) $(@F)
 endif
 endif
 
