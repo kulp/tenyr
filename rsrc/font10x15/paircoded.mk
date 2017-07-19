1cc7f423e846b5db8483bbb0b47329b30f9497e1
Darren Kulp <darren@kulp.ch>
2017-07-18 17:01:53 -0700

    Remove unneeded extra compression with bitflips
    This had never been completely implemented, and in the state it was left
    it was lossy. Since we don't actually need it, strip it out.

--- rsrc/font10x15/paircoded.mk
+++ rsrc/font10x15/paircoded.mk
@@ -8,67 +8,13 @@ allpairs.%: %
 pairlist.memb: $(MEMS:%=uniq.allpairs.%)
 	sort --merge $^ | uniq $(>@)
 
-# flip polarity of first 9 bits, leaving any 10th bit alone (because we assume
-# the 10th bit is always zero as a padding column between glyphs)
-flip.%: %
-	perl -pe 's{([01]{9})([01]|\b)}{ chomp(local $$_ = $$1); y/01/10/; $$_.$$2 }ge' $< $(>@)
-
-# needs GNU head for --quiet
-flips: $(MEMS:%=flip.%)
-	ghead --quiet --lines=14 $^ $(>@)
-
-# retain only bitstrings that begin with `1`
-pre1.%: %
-	grep '^1' $< $(>@)
-
-# find bitstrings that are redundant with their flips
-dups.%: % flip.pre1.%
-	sort $^ | uniq -d $(>@)
-
-# maps something to its negative (with caveats for last column and row being forced to 0)
-# order of the dependencies matters to recursion, so manually swap back the ordering
-map.%: flip.% %
-	paste $^ $(>@)
-
-# turn a tab-separated `map` pair into a perl script that does replacements
-rewrite.%: map.%
-	sed 's#^#s/\\b#; s#	#\\b/#; s#$$#/g;#;' $< $(>@)
-
-# deduplicates lines in % by substituting in the `flip` version based on `map`.
-# We would prefer to use `%` and `map.flip.dups.%` as targets, but Make refuses
-# to recurse through the `flip.%` rule, even though the `%` is different at the
-# different call sites
-swap.%: rewrite.dups.% flip.%
-	perl -p $^ $(>@)
-
-# indexswap.% executes a generated script against % (an index) to replace
-# flippable bitstrings with their flips
-indexswap.%: rewrite.dups.index.memb %
-	perl -p $^ $(>@)
-
-# encode a .memb file pairwise with reference to uniq.indexswap.pairlist.memb
-PAIRINDEX_BITS = $(shell perl -MPOSIX=ceil -e '@a=<>, print ceil(log(@a)/log(2))' uniq.indexswap.pairlist.memb)
-paircoded.%.memb: indexswap.allpairs.%.memb uniq.indexswap.pairlist.memb
+# encode a .memb file pairwise with reference to uniq.pairlist.memb
+PAIRINDEX_BITS = $(shell perl -MPOSIX=ceil -e '@a=<>, print ceil(log(@a)/log(2))' uniq.pairlist.memb)
+paircoded.%.memb: allpairs.%.memb uniq.pairlist.memb
 	cat $< | while read b ; do fgrep -hn "$$b" $(word 2,$^) ; done | cut -d: -f1 | perl -lne 'chomp, print unpack "b$(PAIRINDEX_BITS)", pack "C", $$_-1' $(>@)
 
-# XXX this is not quite right, because we don't have information about
-# indexswap (compressed pairindex vs uncompressed pairindex)
-# TODO keep track during encoding of when we used a flipped index entry, so
-# that we can correctly reconstruct here
-pairdecode.%.memb: paircoded.%.memb uniq.indexswap.pairlist.memb
+pairdecode.%.memb: paircoded.%.memb uniq.pairlist.memb
 	perl -lne 'chomp, print 1+unpack "C", pack "b$(PAIRINDEX_BITS)", $$_' $< | sed 's/$$/p/' | while read b ; do sed -n $$b $(word 2,$^) ; done | tr '\t' '\n' $(>@)
 
-# find lines that are only in the first file
-col1.%: % indexswap.%
-	comm -2 -3 $^ $(>@)
-
-# find line numbers of lines that are only in the first file
-differ.%: col1.% %
-	fgrep -n -f $^ | cut -d: -f1 $(>@)
-
-# turn line numbers of differences into 1-bits
-bitcol1.%: % differ.%
-	(seq 1 $(firstword $(shell wc -l $<)) ; cat $(word 2,$^)) | sort -g | uniq -c | while read a b ; do echo $$(( a == 2 )) ; done $(>@)
-
 clean::
-	$(RM) uniq.indexswap.pairlist.memb pairlist.memb indexswap.pairlist.memb
+	$(RM) uniq.pairlist.memb
