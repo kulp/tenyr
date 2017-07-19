a54e96addd1c540532d7139eca8898688532837c
Darren Kulp <darren@kulp.ch>
2017-07-18 17:27:29 -0700

    Losslessly compress font10x15.memb

--- rsrc/font10x15/paircoded.mk
+++ rsrc/font10x15/paircoded.mk
@@ -5,12 +5,18 @@ uniq.%: %
 allpairs.%: %
 	cat $< | (while read a && read b ; do echo "$$a	$$b" ; done) $(>@)
 
-pairlist.memb: $(MEMS:%=uniq.allpairs.%)
+15to16.%: %
+	perl -lpe 'print "0" x 10 unless $$. % 15' $< $(>@)
+
+16to15.%: %
+	perl -lne 'print unless $$. % 16 == 15' $< $(>@)
+
+pairlist.memb: $(MEMS:%=uniq.allpairs.15to16.%)
 	sort --merge $^ | uniq $(>@)
 
 # encode a .memb file pairwise with reference to uniq.pairlist.memb
 PAIRINDEX_BITS = $(shell perl -MPOSIX=ceil -e '@a=<>, print ceil(log(@a)/log(2))' uniq.pairlist.memb)
-paircoded.%.memb: allpairs.%.memb uniq.pairlist.memb
+paircoded.%.memb: allpairs.15to16.%.memb uniq.pairlist.memb
 	cat $< | while read b ; do fgrep -hn "$$b" $(word 2,$^) ; done | cut -d: -f1 | perl -lne 'chomp, print unpack "b$(PAIRINDEX_BITS)", pack "C", $$_-1' $(>@)
 
 pairdecode.%.memb: paircoded.%.memb uniq.pairlist.memb
