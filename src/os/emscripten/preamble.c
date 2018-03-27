f6743dc66f29d3198c020ffd4d0c7a368ee244ea
Darren Kulp <darren@kulp.ch>
2018-03-26 19:58:59 -0700

    Be stricter about EM_ASM syntax

--- src/os/emscripten/preamble.c
+++ src/os/emscripten/preamble.c
@@ -6,16 +6,16 @@
 int os_preamble()
 {
     if (access("/dev/zero", R_OK))
-        EM_ASM({ FS.createDevice('/dev', 'zero', function () { return 0; }); });
+        EM_ASM(({ FS.createDevice('/dev', 'zero', function () { return 0; }); }),/* dummy arg */0);
 
-    EM_ASM_({
+    EM_ASM_(({
         if (ENVIRONMENT_IS_NODE) {
             var mnt = Pointer_stringify($0);
             FS.mkdir(mnt);
             FS.mount(NODEFS, { root: '/' }, mnt);
             FS.chdir(mnt + process.cwd());
         }
-    }, MOUNT_POINT);
+    }), MOUNT_POINT);
 
     return 0;
 }
