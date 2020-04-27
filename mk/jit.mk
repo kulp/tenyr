471f5b706f4b0fe09a8914f21cf84ef68820abb2
Darren Kulp <darren@kulp.ch>
2020-04-26 18:16:47 -0700

    Suppress pedantry for jit.c

--- mk/jit.mk
+++ mk/jit.mk
@@ -2,6 +2,9 @@ libtenyrjit$(DYLIB_SUFFIX): LDLIBS += -llightning
 libtenyrjit$(DYLIB_SUFFIX): param,dy.o
 libtenyrjit$(DYLIB_SUFFIX): cjit,dy.o
 
+# Explicitly suppress warnings about conversion between function pointers and void*
+jit,dy.o: CFLAGS += -Wno-pedantic
+
 ifneq ($(JIT),0)
 LIB_TARGETS += libtenyrjit$(DYLIB_SUFFIX)
 endif
