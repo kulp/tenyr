d34066d8cc81152a7eded2401dabbc3ea7dbc320
Darren Kulp <darren@kulp.ch>
2020-04-28 05:52:26 -0700

    Address -Wdate-time

--- mk/pedantic.mk
+++ mk/pedantic.mk
@@ -8,6 +8,9 @@ PEDANTIC_FLAGS ?= -Werror -pedantic-errors -Wno-error=unknown-warning-option
 # Unreachable `return` statements after `fatal` are not an error for us.
 PEDANTIC_FLAGS += -Wno-unreachable-code-return
 
+# Our use of __DATE__ and __TIME__ is fine for us.
+PEDANTIC_FLAGS += -Wno-date-time
+
 PEDANTIC_FLAGS += -Werror=covered-switch-default
 PEDANTIC_FLAGS += -Werror=missing-variable-declarations
 PEDANTIC_FLAGS += -Werror=switch-enum
