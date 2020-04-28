474215527bfe208eb2b4ddd4bd03ddc71a0d8992
Darren Kulp <darren@kulp.ch>
2020-04-28 05:46:07 -0700

    Address -Wunreachable-code-return

--- mk/pedantic.mk
+++ mk/pedantic.mk
@@ -5,6 +5,9 @@ PEDANTIC_FLAGS ?= -pedantic
 else
 PEDANTIC_FLAGS ?= -Werror -pedantic-errors -Wno-error=unknown-warning-option
 
+# Unreachable `return` statements after `fatal` are not an error for us.
+PEDANTIC_FLAGS += -Wno-unreachable-code-return
+
 PEDANTIC_FLAGS += -Werror=covered-switch-default
 PEDANTIC_FLAGS += -Werror=missing-variable-declarations
 PEDANTIC_FLAGS += -Werror=switch-enum
