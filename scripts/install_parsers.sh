78487a3fe7b07b1016a504ca7ea974b8cc588026
Darren Kulp <darren@kulp.ch>
2020-05-01 06:49:37 -0700

    Hoist flex/bison-install script

---
+++ scripts/install_parsers.sh
@@ -0,0 +1,12 @@
+case $TRAVIS_OS_NAME in
+    osx)
+        brew install bison
+        BISONS=(/usr/local/Cellar/bison/*/bin/bison)
+        export BISON="${BISONS[${#BISONS[@]}-1]}"
+        ;;
+    windows)
+        choco install winflexbison3
+        export LEX=win_flex
+        export YACC=win_bison
+        ;;
+esac
