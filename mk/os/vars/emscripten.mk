2a7a52e2501f47343fe095f5484cb299fd5b071a
Darren Kulp <darren@kulp.ch>
2020-08-09 16:40:03 -0500

    Depend on NODERAWFS

--- mk/os/vars/emscripten.mk
+++ mk/os/vars/emscripten.mk
@@ -5,6 +5,7 @@ export CC := $(EMCC)
 JIT = 0
 USE_OWN_SEARCH = 1
 LDFLAGS += -s MODULARIZE=1
+LDFLAGS += -s NODERAWFS=1
 DYLIB_SUFFIX = .js
 tpp = cpp
 CHECK_SW_TASKS = check_args check_behaviour check_compile check_sim check_obj check_forth # skip dogfood as it is quite expensive with emscripten
--- src/os/emscripten/preamble.c
+++ src/os/emscripten/preamble.c
@@ -10,16 +10,6 @@ int os_preamble(void)
     if (access("/dev/zero", R_OK))
         EM_ASM(({ FS.createDevice('/dev', 'zero', function () { return 0; }); }),/* dummy arg */0);
 
-    EM_ASM_(({
-        if (ENVIRONMENT_IS_NODE) {
-            var len = 1024; /* arbitrary */
-            var mnt = UTF8ToString($0, len);
-            FS.mkdir(mnt);
-            FS.mount(NODEFS, { root: '/' }, mnt);
-            FS.chdir(mnt + process.cwd());
-        }
-    }), MOUNT_POINT);
-
     return 0;
 }
 
