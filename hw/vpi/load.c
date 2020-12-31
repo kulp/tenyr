bb93fe9569ff0a5a3e4d2f00818a5a7f106486ee
Darren Kulp <darren@kulp.ch>
2020-12-31 14:23:49 -0600

    Allow propagation of failure of tenyr_sim_load

--- hw/vpi/load.c
+++ hw/vpi/load.c
@@ -46,7 +46,6 @@ static int get_range(vpiHandle from, int which)
 PLI_INT32 tenyr_sim_load(PLI_BYTE8 *userdata)
 {
     struct tenyr_sim_state *state = (void*)userdata;
-    int rc = 0;
     vpiHandle array = vpi_handle_by_name("Top.tenyr.ram.store", NULL);
 
     struct dispatch_userdata data = {
@@ -62,27 +61,43 @@ PLI_INT32 tenyr_sim_load(PLI_BYTE8 *userdata)
               args_iter = vpi_iterate(vpiArgument, systfref),
               argh      = vpi_scan(args_iter);
 
-    struct t_vpi_value argval = { .format = vpiStringVal };
-    vpi_get_value(argh, &argval);
-    const char *filename = argval.value.str;
+    int rc = 0;
+    do {
+        struct t_vpi_value argval = { .format = vpiStringVal };
+        vpi_get_value(argh, &argval);
+        vpi_free_object(argh);
+        const char *filename = argval.value.str;
 
-    vpi_free_object(args_iter);
+        FILE *file = fopen(filename, "rb");
+        rc = file == NULL;
+        if (rc)
+            break;
 
-    FILE *file = fopen(filename, "rb");
-    if (!file)
-        return 1;
+        STREAM stream_ = stream_make_from_file(file), *stream = &stream_;
 
-    STREAM stream_ = stream_make_from_file(file), *stream = &stream_;
+        void *ud = NULL;
+        const struct format *f = &tenyr_asm_formats[0];
 
-    void *ud = NULL;
-    const struct format *f = &tenyr_asm_formats[0];
-    if (f->init(stream, NULL, &ud))
-        return 1;
+        rc = f->init(stream, NULL, &ud);
+        if (rc)
+            break;
 
-    rc |= load_sim(vpi_dispatch, &data, f, ud, stream, min);
+        rc |= load_sim(vpi_dispatch, &data, f, ud, stream, min);
 
-    rc |= f->fini(stream, &ud);
+        rc |= f->fini(stream, &ud);
+    } while (0);
 
-    return rc;
+    vpiHandle second = vpi_scan(args_iter);
+    if (second) { // The second argument might not be provided.
+        s_vpi_value retval = {
+            .format = vpiIntVal,
+            .value.integer = rc,
+        };
+
+        vpi_put_value(second, &retval, NULL, vpiNoDelay);
+        vpi_free_object(second);
+    }
+
+    return 0;
 }
 
