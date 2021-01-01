1159aa70d30ef1aad38a35eeb871498c50a91c4f
Darren Kulp <darren@kulp.ch>
2021-01-01 14:43:47 -0600

    Make undefined globals fatal errors

--- src/asmif.c
+++ src/asmif.c
@@ -255,17 +255,15 @@ static int fixup_deferred_exprs(struct parse_data *pd)
     return rc;
 }
 
-static int mark_globals(struct symbol_list *symbols, struct global_list *globals)
+static void mark_globals(struct symbol_list *symbols, struct global_list *globals)
 {
     struct symbol *which;
     list_foreach(global_list, g, globals)
         if ((which = symbol_find(symbols, g->name)))
             which->global = 1;
         else
-            debug(0, "Symbol `%s' was marked as a global, but was not defined",
+            fatal(0, "Symbol `%s' was marked as a global, but was not defined",
                     g->name);
-
-    return 0;
 }
 
 static int check_symbols(struct symbol_list *symbols)
