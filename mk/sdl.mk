555a2fdcfc117681403f561328f256e11f5d6d0f
Darren Kulp <darren@kulp.ch>
2021-03-08 08:19:02 -0500

    Rename sdl2_pkg_config -> sdl2_config

--- mk/sdl.mk
+++ mk/sdl.mk
@@ -1,12 +1,12 @@
 ifneq ($(SDL),0)
-sdl2_pkg_config ?= $(shell $(SDL2_PKGCONFIG) $1 $2)
+sdl2_config ?= $(shell $(SDL2_PKGCONFIG) $1 $2)
 PDEVICES_SDL += sdlled sdlvga
 PDEVICES += $(PDEVICES_SDL)
 # Mark SDL header includes as system includes to avoid subjecting them to full
 # pedantry
 libtenyrsdl%$(DYLIB_SUFFIX) $(PDEVICES_SDL:%=devices/%.d): \
-    CPPFLAGS += $(patsubst -I%,-isystem %,$(call sdl2_pkg_config,--cflags))
-libtenyrsdl%$(DYLIB_SUFFIX): LDLIBS += $(call sdl2_pkg_config,--libs) -lSDL2_image
+    CPPFLAGS += $(patsubst -I%,-isystem %,$(call sdl2_config,--cflags))
+libtenyrsdl%$(DYLIB_SUFFIX): LDLIBS += $(call sdl2_config,--libs) -lSDL2_image
 libtenyrsdlvga$(DYLIB_SUFFIX): | $(TOP)/rsrc/font10x15/invert.font10x15.png
 $(TOP)/rsrc/font10x15/%:
 	$(MAKE) -C $(@D) $(@F)
