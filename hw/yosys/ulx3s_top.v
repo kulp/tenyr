e074ac00d475ead5728a895c07447b153787e1b8
Darren Kulp <darren@kulp.ch>
2021-06-27 20:48:40 -0400

    Wire up GPDI to ulx3s_top
    Hooking up VGA -> GPDI
    
    Generate tmds_pll
    
    Adapted from [an example by Bruno Levy][1].
    
    [1]: https://github.com/BrunoLevy/learn-fpga/tree/e9a82b93f2c1faa73be43b1159073e729c4028d1/Basic/ULX3S/ULX3S_hdmi

--- hw/yosys/ulx3s_top.v
+++ hw/yosys/ulx3s_top.v
@@ -1,7 +1,10 @@
+`default_nettype none
+
 module Top(
     input clk_25mhz,
     input[6:0] btn,
-    output[7:0] led, inout[27:0] gp
+    output[7:0] led, inout[27:0] gp,
+    output [3:0] gpdi_dp
 );
 
     wire clk = clk_25mhz;
@@ -11,7 +14,12 @@ module Top(
     always @(posedge clk)
         startup <= {startup,1'b1};
 
-    Tenyr tenyr(.clk(clk), .reset(reset), .seg(led));
+    wire [2:0] vgaGreen;
+    wire hsync, vsync, inframe;
+
+    Tenyr tenyr(.clk(clk), .reset(reset), .seg(led), .vgaGreen, .hsync, .vsync, .inframe);
+
+    TMDSify gpdi(.clkin(clk), .gpdi_dp, .val(|vgaGreen), .hsync, .vsync, .inframe);
 
     // Force the number of digits in the seg7 display to 1, to prevent
     // digits from being overlaid on top of one another.
