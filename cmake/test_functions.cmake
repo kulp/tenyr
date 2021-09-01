62e2c61a01b711012a2e86de27974ea1d5c0bc4e
Darren Kulp <darren@kulp.ch>
2021-09-01 15:21:44 -0400

    Disable test generation if ICARUS is unset

--- cmake/test_functions.cmake
+++ cmake/test_functions.cmake
@@ -16,45 +16,49 @@ function(check_std_outputs)
         set(FIXTURES_REQUIRED "${COS_INPUT}")
     endif()
 
-    add_test(
-        NAME    "${COS_COMMAND}_${COS_NAME}_create"
-        COMMAND ${CAPTURE_OUTPUT} "${COS_NAME}" ${COS_COMMAND} ${COS_ARGS} "${COS_INPUT}"
-    )
+    if (NOT ICARUS AND COS_COMMAND STREQUAL "vvp")
+        message("Skipping test generation for ${COS_NAME} because ICARUS is not enabled")
+    else()
+        add_test(
+            NAME    "${COS_COMMAND}_${COS_NAME}_create"
+            COMMAND ${CAPTURE_OUTPUT} "${COS_NAME}" ${COS_COMMAND} ${COS_ARGS} "${COS_INPUT}"
+        )
 
-    set_tests_properties(
-        "${COS_COMMAND}_${COS_NAME}_create"
-        PROPERTIES
-            FIXTURES_SETUP "${COS_COMMAND}_${COS_NAME}"
-            FIXTURES_REQUIRED "${FIXTURES_REQUIRED}"
+        set_tests_properties(
+            "${COS_COMMAND}_${COS_NAME}_create"
+            PROPERTIES
+                FIXTURES_SETUP "${COS_COMMAND}_${COS_NAME}"
+                FIXTURES_REQUIRED "${FIXTURES_REQUIRED}"
+                LABELS "tool=${COS_COMMAND}"
+                ${COS_PROPERTIES}
+        )
+
+        add_test(
+            NAME    "${COS_COMMAND}_${COS_NAME}_compare_stdout"
+            COMMAND ${CMAKE_COMMAND} -E compare_files
+                    "${CMAKE_SOURCE_DIR}/test/compare/${COS_COMMAND}/out/${COS_NAME}"
+                    "${COS_NAME}.out"
+        )
+
+        set_tests_properties(
+            "${COS_COMMAND}_${COS_NAME}_compare_stdout"
+            PROPERTIES FIXTURES_REQUIRED "${COS_COMMAND}_${COS_NAME}"
             LABELS "tool=${COS_COMMAND}"
-            ${COS_PROPERTIES}
-    )
+        )
 
-    add_test(
-        NAME    "${COS_COMMAND}_${COS_NAME}_compare_stdout"
-        COMMAND ${CMAKE_COMMAND} -E compare_files
-                "${CMAKE_SOURCE_DIR}/test/compare/${COS_COMMAND}/out/${COS_NAME}"
-                "${COS_NAME}.out"
-    )
+        add_test(
+            NAME    "${COS_COMMAND}_${COS_NAME}_compare_stderr"
+            COMMAND ${CMAKE_COMMAND} -E compare_files
+                    "${CMAKE_SOURCE_DIR}/test/compare/${COS_COMMAND}/err/${COS_NAME}"
+                    "${COS_NAME}.err"
+        )
 
-    set_tests_properties(
-        "${COS_COMMAND}_${COS_NAME}_compare_stdout"
-        PROPERTIES FIXTURES_REQUIRED "${COS_COMMAND}_${COS_NAME}"
-        LABELS "tool=${COS_COMMAND}"
-    )
-
-    add_test(
-        NAME    "${COS_COMMAND}_${COS_NAME}_compare_stderr"
-        COMMAND ${CMAKE_COMMAND} -E compare_files
-                "${CMAKE_SOURCE_DIR}/test/compare/${COS_COMMAND}/err/${COS_NAME}"
-                "${COS_NAME}.err"
-    )
-
-    set_tests_properties(
-        "${COS_COMMAND}_${COS_NAME}_compare_stderr"
-        PROPERTIES FIXTURES_REQUIRED "${COS_COMMAND}_${COS_NAME}"
-        LABELS "tool=${COS_COMMAND}"
-    )
+        set_tests_properties(
+            "${COS_COMMAND}_${COS_NAME}_compare_stderr"
+            PROPERTIES FIXTURES_REQUIRED "${COS_COMMAND}_${COS_NAME}"
+            LABELS "tool=${COS_COMMAND}"
+        )
+    endif()
 
 endfunction()
 
