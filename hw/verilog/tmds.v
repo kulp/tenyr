45fe1555b9e92033bdb4948e0a334269463e5a92
Darren Kulp <darren@kulp.ch>
2026-01-25 19:01:47 -0500

    Use better naming in TMDS

--- hw/verilog/tmds.v
+++ hw/verilog/tmds.v
@@ -9,16 +9,16 @@ module TMDSify(
     wire pixclk;
     wire tmds_clk;
 
-    reg pix;
+    reg pixval;
 
     tmds_pll pll(.clkin, .clkout0(tmds_clk), .clkout1(pixclk));
 
     always @(posedge pixclk)
-        pix <= val;
+        pixval <= val;
 
     wire [9:0] TMDS_grn, TMDS_blu;
-    TMDS_mono_enc encode_grn(.clk(pixclk), .pix, .cd(2'b00)        , .inframe, .tmds(TMDS_grn));
-    TMDS_mono_enc encode_blu(.clk(pixclk), .pix, .cd({vsync,hsync}), .inframe, .tmds(TMDS_blu));
+    TMDS_mono_enc encode_grn(.pixclk(pixclk), .pixval, .vhsyncs(2'b00)        , .inframe, .tmds_parallel(TMDS_grn));
+    TMDS_mono_enc encode_blu(.pixclk(pixclk), .pixval, .vhsyncs({vsync,hsync}), .inframe, .tmds_parallel(TMDS_blu));
 
     reg [9:0] every_ten = 1;
     wire should_load = every_ten[9];
@@ -39,19 +39,19 @@ module TMDSify(
 endmodule
 
 module TMDS_mono_enc(
-        input wire clk,
-        input wire pix,
-        input [1:0] cd,
+        input pixclk,
+        input pixval,
+        input [1:0] vhsyncs,
         input inframe,
-        output reg [9:0] tmds
+        output reg [9:0] tmds_parallel
 );
-    wire [9:0] word = 10'b1100000111;
-    wire [9:0] data = pix ? ~word : word;
-    wire [9:0] base = cd[1] ? 10'b0101010100 : 10'b1101010100;
-    wire [9:0] ctrl = cd[0] ? ~base : base;
+    wire [9:0] blackval = 10'b1100000111;
+    wire [9:0] pixdata = pixval ? ~blackval : blackval;
+    wire [9:0] vsync_ctrl = vhsyncs[1] ? 10'b0101010100 : 10'b1101010100;
+    wire [9:0] hsync_ctrl = vhsyncs[0] ? ~vsync_ctrl : vsync_ctrl;
 
-    always @(posedge clk)
-        tmds <= inframe ? data : ctrl;
+    always @(posedge pixclk)
+        tmds_parallel <= inframe ? pixdata : hsync_ctrl;
 
 endmodule
 
